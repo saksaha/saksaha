c74b946f4bb9f5900f3503e80cd2ec03ec42c740
Elden Park <eldeniyenden@gmail.com>
2021-09-10 17:31:54 +0900

    A

--- saksaha/src/crypto/mod.rs
+++ saksaha/src/crypto/mod.rs
@@ -1,11 +1,7 @@
-use k256::{
-    ecdh::EphemeralSecret,
-    ecdsa::{
+use k256::{EncodedPoint, PublicKey, SecretKey, ecdh::EphemeralSecret, ecdsa::{
         signature::{Signer, Verifier},
         Signature, SigningKey, VerifyingKey,
-    },
-    EncodedPoint, PublicKey, SecretKey,
-};
+    }, elliptic_curve::sec1::ToEncodedPoint};
 use rand_core::OsRng; // requires 'getrandom' feature
 use std::{fmt::Write, num::ParseIntError};
 
@@ -24,8 +20,8 @@ pub fn encode_hex(bytes: &[u8]) -> String {
     s
 }
 
-pub fn generate_key() -> EphemeralSecret {
-    let secret = EphemeralSecret::random(&mut OsRng);
+pub fn generate_key() -> SecretKey {
+    let secret = SecretKey::random(&mut OsRng);
     return secret;
 }
 
@@ -59,41 +55,67 @@ fn a() {
     let alice_secret = EphemeralSecret::random(&mut OsRng);
     let alice_pk_bytes = EncodedPoint::from(alice_secret.public_key());
 
-    // Bob
-    let bob_secret = EphemeralSecret::random(&mut OsRng);
-    let bob_pk_bytes = EncodedPoint::from(bob_secret.public_key());
+    let sk = SecretKey::random(&mut OsRng);
+    let sk_bytes = sk.to_bytes();
 
-    // Alice decodes Bob's serialized public key and computes a shared secret from it
-    let bob_public = PublicKey::from_sec1_bytes(bob_pk_bytes.as_ref())
-        .expect("bob's public key is invalid!"); // In real usage, don't panic, handle this!
+    print!("secret key: {:?}\n", sk);
+    print!("secret key bytes: {:?}\n", alice_pk_bytes);
 
-    let alice_shared = alice_secret.diffie_hellman(&bob_public);
-    let a = alice_shared.as_bytes().as_slice();
+    let sk_rec = SecretKey::from_bytes(sk_bytes).unwrap();
+    print!("recovered secret key {:?}\n", sk_rec);
 
-    let mut f = String::new();
-    for &b in a {
-        write!(&mut f, "{:02x}", b).unwrap();
-    }
+    let public_key = sk.public_key();
+    let enc_point = public_key.to_encoded_point(false);
+    let enc_point_bytes = enc_point.as_bytes();
 
-    // print!("44 {:?}\n", alice_secret);
+    print!("public key: {:?}\n", public_key);
+    print!("encoded point: {:?}\n", enc_point);
+    print!("encoded point as bytes: {:?}\n", enc_point_bytes);
 
-    // c.join(" ");
-    // println!("5 {}", b);
-    // a.join("");
+    let public_key_rec = PublicKey::from_sec1_bytes(enc_point_bytes).unwrap();
+    let enc_point_rec = public_key_rec.to_encoded_point(false);
 
-    print!("31 {:x?}\n", alice_shared.as_bytes());
-    print!("33 {:x?}\n", a);
+    print!("public key rec: {:?}\n", public_key_rec);
+    print!("encoded point rec: {:?}\n", &enc_point_rec);
 
-    // for n in 0..a.
+    assert_eq!(enc_point, enc_point_rec);
+    // PublicKey::fr
 
-    // Bob deocdes Alice's serialized public key and computes the same shared secret
-    let alice_public = PublicKey::from_sec1_bytes(alice_pk_bytes.as_ref())
-        .expect("alice's public key is invalid!"); // In real usage, don't panic, handle this!
+    // // Bob
+    // let bob_secret = EphemeralSecret::random(&mut OsRng);
+    // let bob_pk_bytes = EncodedPoint::from(bob_secret.public_key());
 
-    let bob_shared = bob_secret.diffie_hellman(&alice_public);
+    // // Alice decodes Bob's serialized public key and computes a shared secret from it
+    // let bob_public = PublicKey::from_sec1_bytes(bob_pk_bytes.as_ref())
+    //     .expect("bob's public key is invalid!"); // In real usage, don't panic, handle this!
 
-    print!("44 {:?}\n", bob_shared.as_bytes());
+    // let alice_shared = alice_secret.diffie_hellman(&bob_public);
+    // let a = alice_shared.as_bytes().as_slice();
 
-    // Both participants arrive on the same shared secret
-    assert_eq!(alice_shared.as_bytes(), bob_shared.as_bytes());
+    // let mut f = String::new();
+    // for &b in a {
+    //     write!(&mut f, "{:02x}", b).unwrap();
+    // }
+
+    // // print!("44 {:?}\n", alice_secret);
+
+    // // c.join(" ");
+    // // println!("5 {}", b);
+    // // a.join("");
+
+    // print!("31 {:x?}\n", alice_shared.as_bytes());
+    // print!("33 {:x?}\n", a);
+
+    // // for n in 0..a.
+
+    // // Bob deocdes Alice's serialized public key and computes the same shared secret
+    // let alice_public = PublicKey::from_sec1_bytes(alice_pk_bytes.as_ref())
+    //     .expect("alice's public key is invalid!"); // In real usage, don't panic, handle this!
+
+    // let bob_shared = bob_secret.diffie_hellman(&alice_public);
+
+    // print!("44 {:?}\n", bob_shared.as_bytes());
+
+    // // Both participants arrive on the same shared secret
+    // assert_eq!(alice_shared.as_bytes(), bob_shared.as_bytes());
 }
--- saksaha/src/pconfig/mod.rs
+++ saksaha/src/pconfig/mod.rs
@@ -3,6 +3,7 @@ use crate::{
     crypto,
 };
 use directories::ProjectDirs;
+use k256::elliptic_curve::sec1::ToEncodedPoint;
 use logger::log;
 use std::path::{Path, PathBuf};
 
@@ -19,10 +20,33 @@ pub struct PersistedP2PConfig {
     pub public_key: Option<String>,
 }
 
+enum BookFormat {
+    Paperback,
+    Hardback,
+    Ebook,
+}
+
+struct Book {
+    isbn: i32,
+    format: BookFormat,
+}
+
+// impl PartialEq for Book {
+//     fn eq(&self, other: &Self) -> bool {
+//         self.isbn == other.isbn
+//     }
+// }
+
 impl PConfig {
+
     pub fn new(path: Option<&str>) -> Result<Self, Error> {
         let sk = crypto::generate_key();
-        sk.public_key();
+        let pk = sk.public_key();
+
+        crypto::encode_hex(sk.to_bytes().as_slice());
+        let bb = pk.to_encoded_point(false);
+
+        // crypto::encode_hex(pk.as_affine());
         return Error::result(format!("f"));
     }
 }
