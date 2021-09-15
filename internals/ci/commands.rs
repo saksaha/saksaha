3b0493df3db811e6a47ff1b07f366713ae9208d9
Elden Park <eldeniyenden@gmail.com>
2021-09-15 17:25:39 +0900

    A

--- internals/ci/commands.rs
+++ internals/ci/commands.rs
@@ -1,41 +1,72 @@
-use clap::App;
+use crate::log;
+use clap::{App, Arg, ArgMatches, SubCommand};
 use once_cell::sync::Lazy;
-use std::{
-    sync::{Mutex},
-};
+use std::{process::{Command as StdCommand, Stdio}, sync::Mutex};
 
-static COMMANDS: Lazy<Mutex<Vec<Command>>> = Lazy::new(|| {
+pub static COMMANDS: Lazy<Mutex<Vec<Command>>> = Lazy::new(|| {
     let v = vec![
         Command {
             def: Box::new(dev),
             exec: Box::new(dev_exec),
         },
-        Command {
-            def: Box::new(dev),
-            exec: Box::new(dev_exec),
-        },
+        // Command {
+        //     def: Box::new(dev),
+        //     exec: Box::new(dev_exec),
+        // },
     ];
     Mutex::new(v)
 });
 
-struct Command<'a, 'b> {
-    def: Box<dyn Fn(App<'a, 'b>) -> App<'a, 'b> + Send>,
-    exec: Box<dyn Fn() -> () + Send>,
+pub struct Command<'a, 'b> {
+    pub def: Box<dyn Fn(App<'a, 'b>) -> App<'a, 'b> + Send>,
+    pub exec: Box<dyn Fn(&ArgMatches) -> () + Send>,
+}
+
+fn get_current_dir() -> String {
+    let curr_dir = match std::env::current_dir() {
+        Ok(c) => {
+            let p = c
+                .into_os_string()
+                .into_string()
+                .expect("Current directory needs to be retrieved");
+            p
+        }
+        Err(err) => {
+            log!("Cannot retreive current directory, err: {}\n", err);
+            String::from("")
+        }
+    };
+    curr_dir
 }
 
 fn dev<'a, 'b>(app: App<'a, 'b>) -> App<'a, 'b> {
-    print!("dev\n");
-    return app;
+    app.subcommand(
+        SubCommand::with_name("dev")
+            .version("0.1")
+            .arg(Arg::with_name("args").multiple(true)),
+    )
 }
 
-fn dev_exec() {
-    print!("dev exec\n");
-}
+fn dev_exec(matches: &ArgMatches) {
+    if let Some(matches) = matches.subcommand_matches("dev") {
+        let program = "cargo";
 
-pub fn t() {
-    let a = COMMANDS.lock().unwrap();
-    for x in a.iter() {
-        let b = &x.exec;
-        b();
+        let args = match matches.values_of("args") {
+            Some(a) => {
+                let args: Vec<_> = a.collect();
+                format!("build {}", args.join(" "))
+            }
+            None => String::from("build"),
+        };
+
+        let curr_dir = get_current_dir();
+
+        log!("Executing `{} {}`, curr_dir: {:?}\n", program, args, curr_dir);
+
+        StdCommand::new(program)
+            .arg(args)
+            .stdout(Stdio::inherit())
+            .output()
+            .expect("failed to run");
     }
 }
--- internals/ci/main.rs
+++ internals/ci/main.rs
@@ -1,20 +1,20 @@
-mod macros;
 mod commands;
+mod macros;
 
 use clap::{App, Arg, SubCommand};
-
+use commands::COMMANDS;
 
 fn main() {
     let curr_dir = match std::env::current_dir() {
-        Ok(cd) => {
-            cd.into_os_string().into_string()
-                .expect("current dir must be stringified")
-        },
+        Ok(cd) => cd
+            .into_os_string()
+            .into_string()
+            .expect("current dir must be stringified"),
         Err(_) => {
             log!("Cannot get current working directory\n");
 
             std::process::exit(1);
-        },
+        }
     };
 
     let project_root = match std::env::var("PROJECT_ROOT") {
@@ -37,47 +37,28 @@ fn main() {
     );
 
     if project_root != curr_dir {
-        log!("Warning! You may be running this script not from the project \
-            root with `ci` script");
+        log!(
+            "Warning! You may be running this script not from the project \
+            root with `ci` script"
+        );
     }
 
-    commands::t();
-
-    let matches = App::new("CI")
+    let mut app = App::new("CI")
         .version("0.1")
         .author("Saksaha <team@saksaha.com>")
-        .about("Rust saksaha continuous integration toolsuite")
-        .subcommand(
-            SubCommand::with_name("dev")
-                .version("0.1")
-                .arg(
-                    Arg::with_name("args")
-                        .multiple(true)
-                ),
-        )
-        .get_matches();
+        .about("Rust saksaha continuous integration toolsuite");
 
-        if let Some(o) = matches.value_of("output") {
-            println!("Value for output: {}", o);
-        }
+    let comm = COMMANDS
+        .lock()
+        .expect("cli command definitions must be loaded");
 
-        if let Some(o) = matches.value_of("subcommand") {
-            println!("fff {}", o);
-        }
+    for e in comm.iter() {
+        app = (&e.def)(app);
+    }
 
-        if let Some(matches) = matches.subcommand_matches("dev") {
-            if let Some(args) = matches.values_of("args") {
-                let args: Vec<_> = args.collect();
+    let matches = app.get_matches();
 
-            }
-
-            // print!("{:?}\n", b);
-            // "$ myapp test" was run
-            // if matches.is_present("list") {
-            //     // "$ myapp test -l" was run
-            //     println!("Printing testing lists...");
-            // } else {
-            //     println!("Not printing testing lists...");
-            // }
-        }
+    for e in comm.iter() {
+        (e.exec)(&matches);
+    }
 }
