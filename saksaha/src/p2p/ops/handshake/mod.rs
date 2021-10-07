75c458c147941455f2dee492e9dd74deaa2ff5f9
Elden Park <eldeniyenden@gmail.com>
2021-10-07 23:05:02 +0900

    A

--- saksaha/src/p2p/ops/handshake/mod.rs
+++ saksaha/src/p2p/ops/handshake/mod.rs
@@ -24,57 +24,44 @@ struct Components {
 }
 
 pub struct Handshake {
-    credential: Arc<Credential>,
-    peer_store: Arc<PeerStore>,
-    disc_wakeup_tx: Arc<Sender<usize>>,
-    rpc_port: u16,
     task_mng: Arc<TaskManager>,
-    peer_op_wakeup_rx: Arc<Mutex<Receiver<usize>>>,
 }
 
 impl Handshake {
     pub fn new(
-        peer_store: Arc<PeerStore>,
-        disc_wakeup_tx: Arc<Sender<usize>>,
-        rpc_port: u16,
         task_mng: Arc<TaskManager>,
-        peer_op_wakeup_rx: Arc<Mutex<Receiver<usize>>>,
-        credential: Arc<Credential>,
-        peer_op_listener: TcpListener,
     ) -> Handshake {
         let peer_op = Handshake {
-            credential,
-            peer_store,
-            disc_wakeup_tx,
-            rpc_port,
             task_mng,
-            peer_op_wakeup_rx,
         };
 
         peer_op
     }
 
-    fn make_components(&self) -> Result<Components> {
+    pub async fn start(&self,
+        peer_store: Arc<PeerStore>,
+        disc_wakeup_tx: Arc<Sender<usize>>,
+        rpc_port: u16,
+        peer_op_wakeup_rx: Arc<Mutex<Receiver<usize>>>,
+        credential: Arc<Credential>,
+        peer_op_listener: TcpListener,
+    ) -> Status<u16, Error> {
         let listen = Listen::new(
-            self.disc_wakeup_tx.clone(),
+            disc_wakeup_tx.clone(),
             self.task_mng.clone(),
-            self.credential.clone(),
+            credential.clone(),
         );
 
         let dial = Dial::new(
-            self.credential.clone(),
+            credential.clone(),
             self.task_mng.clone(),
-            self.disc_wakeup_tx.clone(),
-            self.peer_op_wakeup_rx.clone(),
-            self.peer_store.clone(),
+            disc_wakeup_tx.clone(),
+            peer_op_wakeup_rx.clone(),
+            peer_store.clone(),
         );
 
         let components = Components { dial, listen };
 
-        Ok(components)
-    }
-
-    async fn start_components(&self, components: Components) -> Result<u16> {
         let listen = components.listen;
         let listen_start = tokio::spawn(async move {
             return listen.start().await;
@@ -83,9 +70,9 @@ impl Handshake {
         let peer_op_port = match listen_start.await {
             Ok(res) => match res {
                 Ok(port) => port,
-                Err(err) => return Err(err),
+                Err(err) => return Status::SetupFailed(err),
             },
-            Err(err) => return Err(err.into()),
+            Err(err) => return Status::SetupFailed(err.into()),
         };
 
         let dial = components.dial;
@@ -93,20 +80,20 @@ impl Handshake {
             dial.start().await;
         });
 
-        Ok(peer_op_port)
-    }
-
-    pub async fn start(&self) -> Status<u16, Error> {
-        let components = match self.make_components() {
-            Ok(c) => c,
-            Err(err) => return Status::SetupFailed(err),
-        };
-
-        let peer_op_port = match self.start_components(components).await {
-            Ok(port) => port,
-            Err(err) => return Status::SetupFailed(err),
-        };
-
         Status::Launched(peer_op_port)
     }
+
+    // pub async fn start(&self) -> Status<u16, Error> {
+    //     // let components = match self.make_components() {
+    //     //     Ok(c) => c,
+    //     //     Err(err) => return Status::SetupFailed(err),
+    //     // };
+
+    //     let peer_op_port = match self.start_components().await {
+    //         Ok(port) => port,
+    //         Err(err) => return Status::SetupFailed(err),
+    //     };
+
+    //     Status::Launched(peer_op_port)
+    // }
 }
