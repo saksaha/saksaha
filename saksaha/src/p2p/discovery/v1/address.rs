0b821f2d6e8b4dec73aab7e8946fc97f3021e2f0
Elden Park <eldeniyenden@gmail.com>
2021-10-18 18:06:10 +0900

    a

--- saksaha/src/p2p/discovery/v1/address.rs
+++ saksaha/src/p2p/discovery/v1/address.rs
@@ -12,14 +12,10 @@ impl Address {
     fn parse_full_url(url: String) -> Result<Address> {
         let (peer_id, ip, disc_port) = match url.get(6..) {
             Some(u) => match u.split_once('@') {
-                Some((peer_id, endpoint)) => match endpoint.split_once(":") {
-                    Some((ip, port)) => {
-                        (peer_id.to_string(), ip.to_string(), port.to_string())
-                    }
-                    None => {
-                        return err!("url may have illegal ip or port");
-                    }
-                },
+                Some((peer_id, endpoint)) => {
+                    let (ip, port) = parse_endpoint(endpoint)?;
+                    (peer_id.to_string(), ip, port)
+                }
                 None => {
                     return err!("url is not valid, url: {}", url);
                 }
@@ -29,89 +25,29 @@ impl Address {
             }
         };
 
-        let disc_port = match Address::parse_disc_port(disc_port) {
-            Ok(d) => d,
-            Err(err) => return Err(err),
-        };
-
         Ok(Address {
             peer_id,
             ip,
             disc_port,
-            url: url.clone(),
+            url,
         })
     }
 
     fn parse_short_url(url: String) -> Result<Address> {
-
+        let (ip, disc_port) = parse_endpoint(url.as_str())?;
+        Ok(Address {
+            peer_id: "".to_string(),
+            ip,
+            disc_port,
+            url,
+        })
     }
 
     pub fn parse(url: String) -> Result<Address> {
         if url.starts_with("sak://") {
-            return Address::parse_full_url(url)
+            return Address::parse_full_url(url);
         } else {
-            return Address::parse_short_url(url)
-        }
-
-        // let (peer_id, ip, disc_port) = {
-        //     match url.get(6..) {
-        //         Some(u) => match u.split_once('@') {
-        //             Some((peer_id, endpoint)) => {
-        //                 match endpoint.split_once(":") {
-        //                     Some((ip, port)) => (
-        //                         peer_id.to_string(),
-        //                         ip.to_string(),
-        //                         port.to_string(),
-        //                     ),
-        //                     None => {
-        //                         return err!("url may have illegal ip or port");
-        //                     }
-        //                 }
-        //             }
-        //             None => {
-        //                 return err!("url is not valid, url: {}", url);
-        //             }
-        //         },
-        //         None => {
-        //             return err!("url might be of a short form, url: {}", url);
-        //         }
-        //     }
-        // };
-
-        // let disc_port = match disc_port.parse::<u16>() {
-        //     Ok(d) => d,
-        //     Err(err) => {
-        //         return err!(
-        //             "disc port cannot be converted to u16, err: {}, \
-        //                 url: {}, disc_port: {}",
-        //             err,
-        //             url,
-        //             disc_port,
-        //         )
-        //     }
-        // };
-
-        // let addr = Address {
-        //     ip,
-        //     disc_port,
-        //     peer_id,
-        //     url,
-        // };
-
-        // Ok(addr)
-    }
-
-    fn parse_disc_port(disc_port: String) -> Result<u16> {
-        match disc_port.parse::<u16>() {
-            Ok(d) => d,
-            Err(err) => {
-                return err!(
-                    "disc port cannot be converted to u16, err: {}, \
-                        disc_port: {}",
-                    err,
-                    disc_port,
-                )
-            }
+            return Address::parse_short_url(url);
         }
     }
 
@@ -125,5 +61,41 @@ impl Address {
         };
 
         format!("{}@{}:{}", peer_id_short, self.ip, self.disc_port)
+    }
+}
+
+fn parse_endpoint(endpoint: &str) -> Result<(String, u16)> {
+    if endpoint.matches(".").count() < 3 {
+        return err!(
+            "endpoint may not have a valid ip address, endpoint: {}",
+            endpoint
+        );
+    }
+
+    match endpoint.split_once(":") {
+        Some((ip, port)) => {
+            let port = parse_port(port)?;
+            Ok((ip.to_string(), port))
+        }
+        None => {
+            err!(
+                "Error splitting endpoint into ip and port, endpoint: {}",
+                endpoint
+            )
+        }
+    }
+}
+
+fn parse_port(port: &str) -> Result<u16> {
+    match port.parse::<u16>() {
+        Ok(d) => Ok(d),
+        Err(err) => {
+            return err!(
+                "disc port cannot be converted to u16, err: {}, \
+                    disc_port: {}",
+                err,
+                port,
+            )
+        }
     }
 }
