9af5e8d57e1af8481847ad8361eeeb7cde213a7c
Elden Park <eldeniyenden@gmail.com>
2021-11-02 22:21:41 +0900

    c

--- p2p/discovery/src/v1/iterator.rs
+++ p2p/discovery/src/v1/iterator.rs
@@ -1,7 +1,35 @@
-pub struct Iterator {}
+use super::table::Node;
+use std::sync::Arc;
+use tokio::sync::{
+    mpsc::{Receiver, Sender},
+    Mutex,
+};
+
+pub struct Iterator {
+    updates_tx: Arc<Sender<Arc<Node>>>,
+    updates_rx: Arc<Mutex<Receiver<Arc<Node>>>>,
+}
 
 impl Iterator {
-    pub fn new() -> Iterator {
-        Iterator {}
+    pub fn new(
+        updates_tx: Arc<Sender<Arc<Node>>>,
+        updates_rx: Arc<Mutex<Receiver<Arc<Node>>>>,
+    ) -> Iterator {
+        Iterator {
+            updates_tx,
+            updates_rx,
+        }
+    }
+
+    pub async fn next(&self) -> Result<Arc<Node>, String> {
+        let mut updates_rx = self.updates_rx.lock().await;
+        let tnode = match updates_rx.recv().await {
+            Some(n) => n,
+            None => {
+                return Err(format!("Update channel is closed, fatal error"))
+            }
+        };
+
+        Ok(tnode)
     }
 }
