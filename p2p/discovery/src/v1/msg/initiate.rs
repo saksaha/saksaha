bf702ed4955aee4a8554837b551718fd77b5280b
Elden Park <eldeniyenden@gmail.com>
2021-11-02 14:22:01 +0900

    a

--- p2p/discovery/src/v1/msg/initiate.rs
+++
@@ -1,9 +0,0 @@
-pub struct Initiate;
-
-impl Initiate {
-    pub fn run(url: String) -> () {
-        println!("3, {}", url);
-
-        ()
-    }
-}
--- p2p/discovery/src/v1/msg/mod.rs
+++
@@ -1,9 +0,0 @@
-// pub mod codec;
-
-// mod initiate;
-
-// mod receive;
-
-// pub use initiate::Initiate;
-
-// pub use receive::Receive;
