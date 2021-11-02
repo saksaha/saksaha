b24bad7c0608df3adb66bd251ae3c45860b6b6b3
Elden Park <eldeniyenden@gmail.com>
2021-11-03 04:46:40 +0900

    a

--- saksaha/src/p2p/host.rs
+++ saksaha/src/p2p/host.rs
@@ -3,7 +3,7 @@ use super::{
     listener::{self, Listener},
 };
 use crate::{pconfig::PersistedP2PConfig, peer::peer_store::PeerStore};
-use log::error;
+use log::{error, info};
 use saksaha_p2p_discovery::Disc;
 use saksaha_p2p_identity::Identity;
 use std::sync::Arc;
@@ -22,46 +22,73 @@ impl Host {
         bootstrap_urls: Option<Vec<String>>,
         default_bootstrap_urls: &str,
     ) -> Result<Host, String> {
+        let credential = {
+            let secret = p2p_config.secret.to_owned();
+            let public_key = p2p_config.public_key.to_owned();
+
+            let c: Box<dyn Identity + Send + Sync> =
+                match Credential::new(secret, public_key) {
+                    Ok(c) => Box::new(c),
+                    Err(err) => return Err(err),
+                };
+            Arc::new(c)
+        };
+
         let p2p_port = match p2p_port {
             Some(p) => p,
             None => 0,
         };
 
-        let local_addr = format!("127.0.0.1:{}", p2p_port);
+        let (tcp_listener, tcp_port) = {
+            let local_addr = format!("127.0.0.1:{}", p2p_port);
 
-        let (tcp_listener, local_addr) = match TcpListener::bind(local_addr)
-            .await
-        {
-            Ok(listener) => match listener.local_addr() {
-                Ok(local_addr) => {
-                    // log!(DEBUG, "Listener created, addr: {}", local_addr);
+            match TcpListener::bind(local_addr).await {
+                Ok(listener) => match listener.local_addr() {
+                    Ok(local_addr) => {
+                        info!(
+                            "P2P tcp listener is bound, addr: {}",
+                            &local_addr
+                        );
 
-                    (listener, local_addr)
-                }
+                        (listener, local_addr.port())
+                    }
+                    Err(err) => {
+                        return Err(format!(
+                            "Can't get local address of p2p listener, err: {}",
+                            err
+                        ))
+                    }
+                },
                 Err(err) => {
                     return Err(format!(
-                        "Can't get local address of p2p listener, err: {}",
+                        "Can't bind tcp listener, err: {}",
                         err
                     ))
                 }
-            },
-            Err(err) => {
-                return Err(format!("Can't bind tcp listener, err: {}", err))
             }
         };
 
-        let p2p_listener = Listener::new(tcp_listener);
-        let credential = Host::make_credential(p2p_config)?;
-        let peer_store = Host::make_peer_store()?;
+        let peer_store = {
+            let ps = match PeerStore::new(10) {
+                Ok(p) => Arc::new(p),
+                Err(err) => return Err(err),
+            };
+            ps
+        };
 
-        let p2p_listener_port = p2p_listener
-            .start(None, peer_store.clone(), rpc_port, credential.clone())
-            .await?;
+        let p2p_listener = Listener::new(
+            credential.clone(),
+            peer_store.clone(),
+            tcp_listener,
+            rpc_port,
+        );
+
+        p2p_listener.start().await?;
 
         let disc = Disc::init(
             credential.clone(),
             disc_port,
-            p2p_listener_port,
+            tcp_port,
             bootstrap_urls,
             default_bootstrap_urls,
         )
@@ -74,40 +101,12 @@ impl Host {
         Ok(host)
     }
 
-    fn make_credential(
-        p2p_config: PersistedP2PConfig,
-    ) -> Result<Arc<Box<dyn Identity + Send + Sync>>, String> {
-        let secret = p2p_config.secret.to_owned();
-        let public_key = p2p_config.public_key.to_owned();
-
-        let credential = match Credential::new(secret, public_key) {
-            Ok(c) => Box::new(c),
-            Err(err) => return Err(err),
-        };
-
-        Ok(Arc::new(credential))
-    }
-
-    fn make_peer_store() -> Result<Arc<PeerStore>, String> {
-        let peer_store = match PeerStore::new(10) {
-            Ok(p) => Arc::new(p),
-            Err(err) => return Err(err),
-        };
-
-        Ok(peer_store)
-    }
-
     pub async fn start(&self) -> Result<(), String> {
         self.disc.start().await?;
 
         let disc_it = self.disc.iter();
         let a = disc_it.next().await?;
         println!("111,");
-
-        // let dialer = Dialer::new(table);
-        // dialer.schedule();
-
-        // let credential_clone = credential.clone();
 
         // let handshake = Handshake::new(self.task_mng.clone());
         // let handshake_started = handshake.start(
