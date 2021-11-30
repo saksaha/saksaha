ea27172b51f6e0e72b3d660ca64d7c4ff07ce5d7
wonya <wonyeong91@gmail.com>
2021-11-30 09:50:33 +0900

    a
    Signed-off-by: wonya <wonyeong91@gmail.com>

--- example/ci/src/chat.rs
+++ example/ci/src/chat.rs
@@ -1,22 +1,63 @@
-use std::time::Duration;
 use crossterm::style::{Color, SetBackgroundColor};
 use crossterm::terminal::{Clear, ClearType};
 use crossterm::{cursor, execute, QueueableCommand};
 use hyper::Client;
-use tokio::{io::stdin, runtime::Runtime};
+use std::io::{stdin, stdout, Write};
 use std::thread;
-use std::io::{stdout, Write};
+use std::time::Duration;
+use tokio::runtime::Runtime;
+
+enum ChatStatus {
+    Idle,
+    ChatList,
+    ChatStart,
+}
 
 pub fn start_chat() {
+    let mut status = ChatStatus::Idle;
     let mut rt = Runtime::new().unwrap();
-    rt.block_on(async move {
-        tokio::spawn(async {get_new_message().await});
-    });
 
-    loop{
-        // send new messag
-        // stdin()
+    // Do init here
+    status = ChatStatus::ChatList;
+
+    loop {
+        match status {
+            ChatStatus::ChatList => {
+                rt.block_on(async {
+                    get_chat_list().await;
+                });
+                println!("choose chat room number");
+                let mut chat_room_id = String::new();
+                match stdin().read_line(&mut chat_room_id) {
+                    Ok(_) => status = ChatStatus::ChatStart,
+                    Err(err) => println!("invalid chat_room_id {}", err),
+                }
+            }
+            ChatStatus::ChatStart => {
+                rt.block_on(async move {
+                    tokio::spawn(async { get_new_message().await });
+                });
+
+                loop {
+                    // send new messag
+                    let mut buffer = String::new();
+                    stdin().read_line(&mut buffer).expect("invalid message");
+                }
+            }
+            ChatStatus::Idle => {
+                // need init again
+            }
+        }
     }
+}
+
+async fn get_chat_list() {
+    let mut stdout = stdout();
+    let client = Client::new();
+    let bootstrap = "http://google.com".parse::<hyper::Uri>().unwrap();
+    let mut res = client.get(bootstrap).await;
+
+    stdout.write(format!("chat list {:?}", res.unwrap().body()).as_bytes());
 }
 
 async fn get_new_message() {
@@ -26,20 +67,22 @@ async fn get_new_message() {
     execute!(
         stdout,
         Clear(ClearType::All),
-        cursor::MoveTo(0, 0)
-        // SetBackgroundColor(Color::Magenta)
+        cursor::MoveTo(0, 0) // SetBackgroundColor(Color::Magenta)
     );
     loop {
         let client = Client::new();
         let bootstrap = "http://google.com".parse::<hyper::Uri>().unwrap();
         let mut res = client.get(bootstrap).await;
 
+        stdout.queue(cursor::MoveTo(0,0));
         stdout.queue(cursor::SavePosition);
-        stdout.write(
-            format!("New chat {:?}", res.unwrap().body()).as_bytes(),
-        );
-        stdout.queue(cursor::RestorePosition);
+        stdout.write(format!("New chat {:?}", res.unwrap().body()).as_bytes());
+        // should move cursor under current chat
+        stdout.queue(cursor::MoveDown(2));
+        stdout.queue(cursor::MoveLeft(255));
+        stdout.write(format!("Enter new message: ").as_bytes());
         stdout.flush();
+
         thread::sleep(Duration::from_secs(1));
     }
 }
