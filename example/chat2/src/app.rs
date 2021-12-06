e352b4729346381cab3b36f3bf0966ce70829557
Elden Park <eldeniyenden@gmail.com>
2021-12-06 13:18:44 +0900

    a

--- example/chat2/src/app.rs
+++ example/chat2/src/app.rs
@@ -1,9 +1,22 @@
+use crate::chat::Chat;
+use crate::data::{client_0, client_1};
+use bytes::BytesMut;
+use ff::PrimeField;
+use ff::{Field, PrimeFieldBits};
+use proofs::constants::get_round_constants;
+use proofs::{get_merkle_tree, verify_proof};
+use rand::prelude::ThreadRng;
+use rand::rngs::OsRng;
+use rand::thread_rng;
+use rsa::pkcs8::{FromPrivateKey, ToPrivateKey};
+use rsa::{PaddingScheme, PublicKey, RsaPrivateKey, RsaPublicKey};
+use serde::{Deserialize, Serialize};
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::{TcpListener, TcpSocket, TcpStream};
 use tokio::sync::Mutex;
 use tokio::{self, signal};
 
-use crate::chat::Chat;
-use crate::data::{client_0, client_1};
+const KEY: &str = "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDHhNucfrRmy9vd\nctDR+Lhf5GMljv5HmUFNQ7rfB8wqH+Zqb/PMB89zrrtWOCnwsqbbQKW1fJoe41Ad\nDfr+F5M/CWwTZQeKsRJkEzaILd4DZ9dZ2t+Y+6+LaXQqpe4+BvGhFufwiZk5O/Pn\nhQRNVW98ttVkWYZ3+JG/u/ncDovufJKVRuFXJkCCEp4pd8SNRLUz6ALapm5x1hyZ\nxKZO8Yh4/Vrm85v22gSFGDz3vtElctHTPCoD8UTe2JtAE6doEfp2I5BhNCDFv6Z+\nqYXdSYTTNoLNzV93JqupaXo3eVd8pha4SyDi2vZ4CTu2fCvDd5bteOHHCVVlEVs1\nsyIubV6tAgMBAAECggEBAJKetsiFcEtOqm9Nbdmv3sZRTu8N6x1wqUV/bXdzAn0+\nXhR/2vhoEPKhSHgu49tHXkknc98wBw6F25zOZlGNv56GbQ6/otAwGiTC3cMrecik\n6ePVbZW9954Ky9x/fdnFPpI0BBSG+bvOrAnKfYPbMgfDUIPjKmdFms389LvbUMfb\nJE4pRZn7oOjAzp5dX0xojvS1JARsbqxYtWIwGuOFNno1k+q4fm4VijfrR4cZ+OA1\nb7xPVW6qyaEEQ/8ZPlSTvaKP4jChUcKHDOP0qRWdUAsWXmoTwebgl6OVsUAJnaRx\nTgcklmYDhOOcFYYIjwLbLPJvOlsBtFG+3cIEC9XM3j0CgYEAzligZTKV36++eIl2\nAp1l9rrRARD43l74VJsxB+4NyHd3+xyVE4s8YUiHHmtmCDsQS5pEvmcsV2jSYnD4\nFUsVE70jr5kxUdkXv8+g/OYmYbrgmAaH+V7dIRtAPd1X50w+clviuxNkSxahfUU5\nDah6OJbAVj6Zcsic+ETHKCd4OpcCgYEA94eo33N3Uk2MhW+ixXOjCy38kbJfVt01\nqJinkT2UBdVewEVIZfkLqWJaJjojz4WTHRao7CAVEV+lhwppWE2B55kdR4hW0rW7\nVRYy89kbnLYwHaqq3dgwtw+jwk3hDESWjd5icl+A5v5BWslnR1taqtNbntqDcG/i\nFc72Sh88LVsCgYApUxyER9Y2HxcxQq5MpcMW36Ed6o08054+K3ptUSKXhD2WH8pH\nSLpF7WWKFQ4xFcsUqQedbkI2GzsPFfrJIOlOs7Fi6HY1IxobgvSF0X3cUrXKHHy8\n11/H0pJSEXMaUm2rL3W/64lJdHXRBb0AXD5l+OL6Ir1OfJjbK8MK6qSsbwKBgEc8\nrpgoVeXE1bUtTK4AuqKkCHUx3YhVP3UXAdql2yzWnF8/UTevtfB4krM/tkCqE42i\nkNjr3sRSP4QetfSo0cMCaM/GaB5aGODC88UD7ZNrR2uyS1xvNI3qnVl1KlhqgN2x\na6m43JMeB1cGcYeyveQ90bdk8cDxIiLenjj0xk/TAoGBAMYM+CuegiEIQCDOIN5l\nQ+ijuL5Levwb2D8ZA9U0jDiUsoNFwyOox2DWsLI7DTO3O2pvm/ODlP+YsaeVYYC+\nyQKlE2+jlLMiMmH8yTk+nGs+GQaAvULE8fOFhEuLaVTV9IsH/39n25x0wgS1Zacu\nQG9GO7oa7XbW4rRgH+VBptow\n-----END PRIVATE KEY-----\n";
 
 pub struct ChatApp {
     cid: String,
@@ -55,7 +68,17 @@ impl ChatApp {
 }
 
 async fn start_routine(cid: String) {
+
     if cid.clone() == "0" {
+        let priv_key =
+            RsaPrivateKey::from_pkcs8_pem(&KEY).expect("failed to generate a key");
+        let pub_key = RsaPublicKey::from(&priv_key);
+        let channel_data = b"channel_id";
+        let padding = PaddingScheme::new_pkcs1v15_encrypt();
+        let channel = pub_key
+            .encrypt(&mut OsRng, padding, &channel_data[..])
+            .expect("channel should be encrypted");
+
         let my_ip = client_0::IP;
         let my_port = client_0::PORT;
 
@@ -106,12 +129,24 @@ async fn start_routine(cid: String) {
             let mut chat = Chat {
                 cid: cid.clone(),
                 stream,
+                priv_key,
+                pub_key,
+                channel,
             };
             chat.start().await;
         });
     }
 
     if cid == "1" {
+        let priv_key =
+            RsaPrivateKey::from_pkcs8_pem(&KEY).expect("failed to generate a key");
+        let pub_key = RsaPublicKey::from(&priv_key);
+        let channel_data = b"channel_id";
+        let padding = PaddingScheme::new_pkcs1v15_encrypt();
+        let channel = pub_key
+            .encrypt(&mut OsRng, padding, &channel_data[..])
+            .expect("channel should be encrypted");
+
         let dst_id = client_0::IP;
         let dst_port = client_0::PORT;
 
@@ -138,6 +173,9 @@ async fn start_routine(cid: String) {
         let mut chat = Chat {
             cid: cid.clone(),
             stream,
+            priv_key,
+            pub_key,
+            channel,
         };
         chat.start().await;
     }
--- example/chat2/src/chat.rs
+++ example/chat2/src/chat.rs
@@ -1,25 +1,44 @@
-use bytes::BytesMut;
-use proofs::constants::get_round_constants;
-use proofs::{verify_proof, get_merkle_tree};
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
-use tokio::net::TcpStream;
-
 use bellman::gadgets::boolean::{AllocatedBit, Boolean};
 use bellman::groth16::{Parameters, Proof};
 use bellman::{groth16, Circuit, ConstraintSystem, SynthesisError};
 use bls12_381::{Bls12, MillerLoopResult, Scalar};
+use bytes::BytesMut;
 use ff::PrimeField;
 use ff::{Field, PrimeFieldBits};
+use proofs::constants::get_round_constants;
+use proofs::{get_merkle_tree, verify_proof};
+use rand::prelude::ThreadRng;
 use rand::rngs::OsRng;
 use rand::thread_rng;
+use rsa::pkcs8::{FromPrivateKey, ToPrivateKey};
+use rsa::{PaddingScheme, PublicKey, RsaPrivateKey, RsaPublicKey};
+use serde::{Deserialize, Serialize};
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::TcpStream;
+
 
 pub struct Chat {
     pub cid: String,
     pub stream: TcpStream,
+    pub priv_key: RsaPrivateKey,
+    pub pub_key: RsaPublicKey,
+    pub channel: Vec<u8>,
 }
 
 impl Chat {
     pub async fn start(&mut self) {
+        // let priv_key = RsaPrivateKey::from_pkcs8_pem(&KEY)
+        //     .expect("failed to generate a key");
+        // let pub_key = RsaPublicKey::from(&priv_key);
+        // let channel_data = b"channel_id";
+        // let padding = PaddingScheme::new_pkcs1v15_encrypt();
+        // let channel = pub_key
+        //     .encrypt(&mut OsRng, padding, &channel_data[..])
+        //     .expect("channel should be encrypted");
+
+        println!("Shared Private key: {:?}", self.priv_key);
+        println!("Channel id: {:?}", self.channel);
+
         let tree = get_merkle_tree(&get_round_constants());
         println!("[ledger] Transactions up to so far: {:?}\n", tree.data);
 
@@ -31,7 +50,7 @@ impl Chat {
                 .read_line(&mut buffer)
                 .expect("invalid message");
 
-            print!("You: {}", buffer);
+            print!("You: {} [channel: {:?}]", buffer, self.channel);
 
             let write_buf = buffer.as_bytes();
             self.stream.write(&write_buf[..]).await;
@@ -56,7 +75,7 @@ impl Chat {
                 }
             };
 
-            print!("Her: {}", msg);
+            print!("Her: {} [channel: {:?}]", msg, self.channel);
         }
     }
 }
