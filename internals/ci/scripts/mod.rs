f5a8d6219d1d8f0bde1f83ceb9cfa8b198008140
Elden Park <eldeniyenden@gmail.com>
2022-04-15 18:50:07 +0900

    r

--- internals/ci/scripts/mod.rs
+++ internals/ci/scripts/mod.rs
@@ -1,4 +1,5 @@
 use crate::scriptify::Scriptify;
+use clap::{Arg, ArgMatches, Command};
 
 mod build;
 mod clean;
@@ -9,16 +10,40 @@ mod postcommit;
 mod run;
 mod test;
 
-pub(crate) fn get_scripts() -> Vec<Box<dyn Scriptify + Send>> {
-    let scripts: Vec<Box<dyn Scriptify + Send>> = vec![
-        Box::new(build::Build),
-        Box::new(dev::Dev),
-        Box::new(run::Run),
-        Box::new(clean::Clean),
-        Box::new(expand::Expand),
-        Box::new(expand_release::ExpandRelease),
-        Box::new(postcommit::PostCommit),
-        Box::new(test::Test),
-    ];
-    scripts
+// pub(crate) fn get_scripts() -> Vec<Box<dyn Scriptify + Send>> {
+//     let scripts: Vec<Box<dyn Scriptify + Send>> = vec![
+//         Box::new(build::Build),
+//         Box::new(dev::Dev),
+//         Box::new(run::Run),
+//         Box::new(clean::Clean),
+//         Box::new(expand::Expand),
+//         Box::new(expand_release::ExpandRelease),
+//         Box::new(postcommit::PostCommit),
+//         Box::new(test::Test),
+//     ];
+//     scripts
+// }
+
+type ScriptMainFunction = fn(matches: &ArgMatches);
+
+type ScriptDef = (&'static str, Script);
+
+const a: ScriptMainFunction = build::build;
+
+pub(crate) enum Script {
+    Build(&'static str, ScriptMainFunction),
+    Dev(&'static str, ScriptMainFunction),
+}
+
+// impl Script {
+//     fn value(&self) {
+//         match *self {}
+//     }
+// }
+
+pub(crate) fn get_scripts<'a>() -> Vec<Script> {
+    vec![
+        Script::Build("build", build::build),
+        Script::Dev("dev", dev::dev),
+    ]
 }
