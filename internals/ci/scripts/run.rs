e6acb10e624f3d24700ebdd80116c4a8ca350068
Elden Park <eldeniyenden@gmail.com>
2022-04-20 18:56:16 +0900

    o

--- internals/ci/scripts/run.rs
+++ internals/ci/scripts/run.rs
@@ -15,27 +15,23 @@ impl Script for Run {
     }
 
     fn handle_matches(&self, matches: &ArgMatches) -> Option<bool> {
-        if let Some(matches) = matches.subcommand_matches(self.name()) {
-            let program = "cargo";
-            let args = match matches.values_of("args") {
-                Some(a) => a.collect(),
-                None => vec![],
-            };
-            let args = [vec!["run", "--release", "-p", "saksaha", "--"], args]
-                .concat();
+        let program = "cargo";
+        let args = match matches.values_of("args") {
+            Some(a) => a.collect(),
+            None => vec![],
+        };
+        let args =
+            [vec!["run", "--release", "-p", "saksaha", "--"], args].concat();
 
-            log!("Executing `{} {:?}`", program, args);
+        log!("Executing `{} {:?}`", program, args);
 
-            Cmd::new(program)
-                .args(args)
-                .stdout(Stdio::inherit())
-                .stderr(Stdio::inherit())
-                .output()
-                .expect("failed to run");
+        Cmd::new(program)
+            .args(args)
+            .stdout(Stdio::inherit())
+            .stderr(Stdio::inherit())
+            .output()
+            .expect("failed to run");
 
-            return Some(true);
-        }
-
-        None
+        return Some(true);
     }
 }
