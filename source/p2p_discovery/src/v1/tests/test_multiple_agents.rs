09884677a51d95100d242857c94dbf4a8761a7f5
Elden Park <eldeniyenden@gmail.com>
2022-05-18 15:23:43 +0900

    o

--- source/p2p_discovery/src/v1/tests/test_multiple_agents.rs
+++ source/p2p_discovery/src/v1/tests/test_multiple_agents.rs
@@ -1,197 +1,29 @@
+use super::*;
+
 #[cfg(test)]
 mod test {
-    use crate::table::AddrVal;
+    use super::utils;
+    use crate::AddrVal;
     use crate::Discovery;
     use crate::DiscoveryArgs;
     use p2p_identity::addr::AddrStatus;
     use p2p_identity::addr::UnknownAddr;
     use p2p_identity::identity::P2PIdentity;
     use std::sync::Arc;
+    use std::time::Duration;
 
-    fn init() {
-        let _ = env_logger::builder().is_test(true).try_init();
-    }
-
-    async fn create_client_1() -> Arc<Discovery> {
+    pub(super) async fn create_4_to_1_client_1() -> Arc<Discovery> {
         let secret = String::from(
             "aa99cfd91cc6f3b541d28f3e0707f9c7bcf05cf495308294786ca450b501b5f2",
         );
 
         let public_key_str = String::from(
             "\
-            04240874d8c323c22a571f735e835ed2\
-            f0619893a3989e557b1c9b4c699ac92b\
-            84d0dc478108629c0353f2876941f90d\
-            4b36346bcc19c6b625422adffb53b3a6af\
-            ",
-        );
-
-        let bootstrap_addrs = vec![UnknownAddr {
-            ip: String::from("127.0.0.1"),
-            disc_port: 35519,
-            p2p_port: None,
-            sig: None,
-            public_key_str: Some(String::from(
-                "\
-                04ce80d8c998044270b26eb7597bd92\
-                eb188807ace620644a34bf3be145422e\
-                61af51724079002c17758c33b88ade2e\
-                789a2153c1fd5b808c1f971127c2592009a\
+                04240874d8c323c22a571f735e835ed2\
+                f0619893a3989e557b1c9b4c699ac92b\
+                84d0dc478108629c0353f2876941f90d\
+                4b36346bcc19c6b625422adffb53b3a6af\
                 ",
-            )),
-            status: AddrStatus::Initialized,
-        }];
-
-        let p2p_identity = {
-            let id = P2PIdentity::new(secret, public_key_str).unwrap();
-            Arc::new(id)
-        };
-
-        let disc_args = DiscoveryArgs {
-            disc_dial_interval: None,
-            disc_table_capacity: None,
-            disc_task_interval: None,
-            disc_task_queue_capacity: None,
-            p2p_identity: p2p_identity.clone(),
-            disc_port: Some(35520),
-            p2p_port: 1,
-            bootstrap_addrs,
-        };
-
-        let p2p_discovery = {
-            let d = Discovery::init(disc_args)
-                .await
-                .expect("Discovery should be initailized");
-            Arc::new(d)
-        };
-
-        p2p_discovery
-    }
-
-    async fn create_client_2() -> Arc<Discovery> {
-        let secret = String::from(
-            "445f36a1f7fafce4408b2277a5d009d1f1ba452d3996bfe7136de4adbfa34d61",
-        );
-
-        let public_key_str = String::from(
-            "\
-            04ce80d8c998044270b26eb7597bd92e\
-            b188807ace620644a34bf3be145422e6\
-            1af51724079002c17758c33b88ade2e7\
-            89a2153c1fd5b808c1f971127c2592009a\
-            ",
-        );
-
-        let bootstrap_addrs = vec![];
-
-        let p2p_identity = {
-            let id = P2PIdentity::new(secret, public_key_str).unwrap();
-            Arc::new(id)
-        };
-
-        let disc_args = DiscoveryArgs {
-            disc_dial_interval: None,
-            disc_table_capacity: None,
-            disc_task_interval: None,
-            disc_task_queue_capacity: None,
-            p2p_identity: p2p_identity.clone(),
-            disc_port: Some(35519),
-            p2p_port: 2,
-            bootstrap_addrs,
-        };
-
-        let p2p_discovery = {
-            let d = Discovery::init(disc_args)
-                .await
-                .expect("Discovery should be initailized");
-            Arc::new(d)
-        };
-
-        p2p_discovery
-    }
-
-    #[tokio::test(flavor = "multi_thread")]
-    async fn test_addr_is_back_in_the_queue() {
-        init();
-
-        let disc_1 = create_client_1().await;
-        let disc_2 = create_client_2().await;
-
-        let disc_1_clone = disc_1.clone();
-        let disc_2_clone = disc_2.clone();
-
-        tokio::spawn(async move {
-            println!("running disc_1");
-            disc_1_clone.run().await;
-        });
-        tokio::spawn(async move {
-            println!("running disc_2");
-            disc_2_clone.run().await;
-        });
-
-        let disc_1_clone = disc_1.clone();
-
-        let addr_iter_thread = tokio::spawn(async move {
-            let iter = disc_1_clone.new_iter();
-
-            let known_addr_ip: String;
-            let known_addr_disc_port: u16;
-
-            {
-                let addr_guard =
-                    iter.next().await.expect("Address should be popped");
-
-                let addr = addr_guard.addr.write().await;
-                let known_addr = match &addr.val {
-                    AddrVal::Known(k) => k,
-                    _ => panic!("Known addr should be provided"),
-                };
-
-                known_addr_ip = known_addr.ip.clone();
-                known_addr_disc_port = known_addr.disc_port.clone();
-
-                log::info!("Popped addr, {}", known_addr);
-            }
-
-            let addr_guard =
-                iter.next().await.expect("Address should be popped");
-
-            let addr = addr_guard.addr.write().await;
-            let known_addr = match &addr.val {
-                AddrVal::Known(k) => k,
-                _ => panic!("Known addr should be provided"),
-            };
-
-            log::info!("Popped addr, {}", known_addr);
-
-            log::info!(
-                "{:?} == {:?}",
-                (&known_addr_ip, known_addr_disc_port),
-                (known_addr.ip.clone(), known_addr.disc_port)
-            );
-
-            assert_eq!(
-                (known_addr_ip, known_addr_disc_port),
-                (known_addr.ip.clone(), known_addr.disc_port)
-            );
-        });
-
-        tokio::join!(addr_iter_thread);
-    }
-
-    // test_discovery_4_to_1
-    async fn create_4_to_1_client_1() -> Arc<Discovery> {
-        let secret = String::from(
-            "aa99cfd91cc6f3b541d28f3e0707f9c7bcf05cf495308294786ca450b501b5f2",
-        );
-
-        let public_key_str = String::from(
-            "\
-            04240874d8c323c22a571f735e835ed2\
-            f0619893a3989e557b1c9b4c699ac92b\
-            84d0dc478108629c0353f2876941f90d\
-            4b36346bcc19c6b625422adffb53b3a6af\
-            ",
         );
 
         // itself
@@ -202,11 +34,11 @@ mod test {
             sig: None,
             public_key_str: Some(String::from(
                 "\
-            04240874d8c323c22a571f735e835ed2\
-            f0619893a3989e557b1c9b4c699ac92b\
-            84d0dc478108629c0353f2876941f90d\
-            4b36346bcc19c6b625422adffb53b3a6af\
-            ",
+                04240874d8c323c22a571f735e835ed2\
+                f0619893a3989e557b1c9b4c699ac92b\
+                84d0dc478108629c0353f2876941f90d\
+                4b36346bcc19c6b625422adffb53b3a6af\
+                ",
             )),
             status: AddrStatus::Initialized,
         }];
@@ -445,9 +277,10 @@ mod test {
         p2p_discovery
     }
 
+    // {2,3,4,5} => {1}
     #[tokio::test(flavor = "multi_thread")]
     async fn test_discovery_4_to_1() {
-        init();
+        utils::init();
 
         let disc_1 = create_4_to_1_client_1().await;
         let disc_2 = create_4_to_1_client_2().await;
@@ -461,33 +294,69 @@ mod test {
         let disc_4_clone = disc_4.clone();
         let disc_5_clone = disc_5.clone();
 
-        let mut handles = vec![];
+        println!("All discs are initialized");
 
-        handles.push(tokio::spawn(async move {
+        let disc_1_thread = tokio::spawn(async move {
             println!("running disc_1");
             disc_1_clone.run().await;
-        }));
+        });
 
-        handles.push(tokio::spawn(async move {
+        let disc_2_thread = tokio::spawn(async move {
             println!("running disc_2");
             disc_2_clone.run().await;
-        }));
+        });
 
-        handles.push(tokio::spawn(async move {
+        let disc_3_thread = tokio::spawn(async move {
             println!("running disc_3");
             disc_3_clone.run().await;
-        }));
+        });
 
-        handles.push(tokio::spawn(async move {
+        let disc_4_thread = tokio::spawn(async move {
             println!("running disc_4");
             disc_4_clone.run().await;
-        }));
+        });
 
-        handles.push(tokio::spawn(async move {
+        let disc_5_thread = tokio::spawn(async move {
             println!("running disc_5");
             disc_5_clone.run().await;
-        }));
+        });
 
-        futures::future::join_all(handles).await;
+        let disc_1_clone = disc_1.clone();
+        let test_thread = tokio::spawn(async move {
+            println!("Starting test thread, sleeping for 5 seconds");
+
+            tokio::time::sleep(Duration::from_secs(5)).await;
+
+            println!("Test thread waken up");
+
+            let iter = disc_1_clone.new_iter();
+            let mut count = 0;
+
+            while count < 5 {
+                println!("getting next address");
+                let addr_guard = iter.next().await.unwrap();
+                println!("acquired next address!");
+
+                let addr_lock = addr_guard.addr.read().await;
+                match &addr_lock.val {
+                    AddrVal::Known(k) => {
+                        k.p2p_endpoint();
+                    }
+                    AddrVal::Unknown(u) => {}
+                };
+                count += 1;
+            }
+
+            println!("Test succeeded!");
+        });
+
+        let _ = tokio::join!(
+            disc_1_thread,
+            disc_2_thread,
+            disc_3_thread,
+            disc_4_thread,
+            disc_5_thread,
+            test_thread,
+        );
     }
 }
