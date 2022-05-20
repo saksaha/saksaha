cdaf9b94f7c5d9b64e02f35323c00ad310e3268c
Elden Park <eldeniyenden@gmail.com>
2022-05-20 16:08:00 +0900

    o

--- source/saksaha/src/blockchain/blockchain.rs
+++ source/saksaha/src/blockchain/blockchain.rs
@@ -10,7 +10,7 @@ pub(crate) struct Blockchain {
 }
 
 impl Blockchain {
-    pub async fn init(
+    pub(crate) async fn init(
         ledger_db_path: Option<String>,
     ) -> Result<Blockchain, String> {
         let ledger = Ledger::init(ledger_db_path).await?;
@@ -22,10 +22,14 @@ impl Blockchain {
         Ok(blockchain)
     }
 
-    pub async fn run(&self) {
+    pub(crate) async fn run(&self) {
         tinfo!("saksaha", "blockchain", "Start running blockchain");
 
         self.ledger.write_tx();
         self.ledger.read_tx();
     }
+
+    pub(crate) async fn _send_transaction() {}
+
+    pub(crate) async fn _get_transaction() {}
 }
--- source/saksaha/src/blockchain/tests/mod.rs
+++ source/saksaha/src/blockchain/tests/mod.rs
@@ -2,7 +2,6 @@ use super::*;
 
 #[cfg(test)]
 mod test {
-    use super::dummy::Transaction;
     use crate::blockchain::{ledger::ledger_columns, Blockchain};
     use chrono::{DateTime, Utc};
     use file_system::FS;
@@ -10,14 +9,18 @@ mod test {
     use rocksdb::{DBWithThreadMode, SingleThreaded, WriteBatch};
     use sha3::{Digest, Sha3_256};
 
+    struct TxValue<'a> {
+        created_at: &'a str,
+        data: &'a str,
+        pi: &'a str,
+        sig_vec: &'a str,
+    }
+
     fn init() {
         let _ = env_logger::builder().is_test(true).try_init();
     }
 
-    #[tokio::test(flavor = "multi_thread")]
-    async fn test_send_transaction() {
-        init();
-
+    async fn make_blockchain() -> Blockchain {
         let db_path = {
             let app_path = FS::create_or_get_app_path().unwrap();
             let db_path = app_path.join("db_ledger_test");
@@ -25,74 +28,126 @@ mod test {
             Some(db_path)
         };
 
-        let blockchain = Blockchain::init(db_path).await.unwrap();
+        let blockchain = Blockchain::init(db_path)
+            .await
+            .expect("Blockchain should be initialized");
+
+        blockchain
+    }
+
+    fn make_dummy_values() -> Vec<(String, TxValue<'static>)> {
+        let tx = TxValue {
+            pi: "0x123",
+            sig_vec: "0x0000",
+            created_at: "1346546123",
+            data: "None",
+        };
+
+        let transaction_data = vec!["foo", "bob", "sandy", "land", "god"];
+
+        let mut values = vec![];
+
+        for (_idx, tx_data) in transaction_data.iter().enumerate() {
+            let tx_hash = {
+                let mut h = Sha3_256::new();
+                h.update(tx_data);
+                h.finalize()
+            };
+
+            let tx_hash_str = hex::encode(tx_hash);
+
+            let tx_value = TxValue {
+                pi: tx.pi,
+                sig_vec: tx.sig_vec,
+                created_at: tx.created_at,
+                data: tx.data,
+            };
+
+            values.push((tx_hash_str, tx_value))
+        }
+
+        values
+    }
+
+    #[tokio::test(flavor = "multi_thread")]
+    async fn test_put_and_get_transaction() {
+        init();
+
+        let blockchain = make_blockchain().await;
 
         let db = blockchain.ledger.ledger_db.db;
 
-        let tx =
-            Transaction::new("0x0000", "0x123", "0x0000", "1346546123", "None");
+        // let tx =
+        //     Transaction::new("0x0000", "0x123", "0x0000", "1346546123", "None");
 
-        let foo = vec!["foo", "bob", "sandy", "land", "god"];
-        for (_idx, foo) in foo.iter().enumerate() {
-            let mut tx_hash = Sha3_256::new();
-            tx_hash.update(foo);
-            let tx_hash = tx_hash.finalize();
-            let hash_str = hex::encode(tx_hash);
+        let dummy_tx_values = make_dummy_values();
 
-            let cf_val = vec![
-                // (ledger_columns::TX_HASH, &hash_str[..], &hash_str[..]),
-                (ledger_columns::PI, tx.pi),
-                (ledger_columns::SIG_VEC, tx.sig_vec),
-                (ledger_columns::CREATED_AT, tx.created_at),
-                (ledger_columns::DATA, tx.data),
-            ];
+        dummy_tx_values.iter().for_each(|(tx_hash, tx_val)| {
+            db.put_cf(
+                db.cf_handle(ledger_columns::CREATED_AT).unwrap(),
+                tx_hash,
+                tx_val.created_at,
+                //
+            )
+            .expect("Tx crated_at should be inserted");
 
-            // put_and_get_transaction(&db, &cf_val);
-            batchput_and_get_transaction(
-                &db,
-                &cf_val,
-                &hash_str,
-                Utc::now().timestamp(),
+            db.put_cf(
+                db.cf_handle(ledger_columns::DATA).unwrap(),
+                tx_hash,
+                tx_val.data,
+                //
+            )
+            .expect("Tx data should be inserted");
+
+            db.put_cf(
+                db.cf_handle(ledger_columns::PI).unwrap(),
+                tx_hash,
+                tx_val.pi,
+                //
+            )
+            .expect("Tx pi should be inserted");
+
+            db.put_cf(
+                db.cf_handle(ledger_columns::SIG_VEC).unwrap(),
+                tx_hash,
+                tx_val.sig_vec,
+                //
+            )
+            .expect("Tx sig_vec should be inserted");
+        });
+
+        dummy_tx_values.iter().for_each(|(tx_hash, _)| {
+            let created_at = db
+                .get_cf(
+                    db.cf_handle(ledger_columns::CREATED_AT).unwrap(),
+                    tx_hash,
+                )
+                .expect("created_at should be returned")
+                .expect("created_at should exist");
+
+            println!(
+                "key: {}, got value: {:?}",
+                ledger_columns::CREATED_AT,
+                std::str::from_utf8(&created_at)
             );
-        }
-        let cf_val = vec![
-            ledger_columns::TX_HASH,
-            ledger_columns::PI,
-            ledger_columns::SIG_VEC,
-            ledger_columns::CREATED_AT,
-            ledger_columns::DATA,
-        ];
-        raw_iterator_to_first(&db, cf_val);
-        // println!("now: {}", )
+
+            let created_at = db
+                .get_cf(
+                    db.cf_handle(ledger_columns::CREATED_AT).unwrap(),
+                    tx_hash,
+                )
+                .expect("created_at should be returned")
+                .expect("created_at should exist");
+
+            println!(
+                "key: {}, got value: {:?}",
+                ledger_columns::CREATED_AT,
+                std::str::from_utf8(&created_at)
+            );
+        });
     }
 
-    fn put_and_get_transaction(
-        db: &DBWithThreadMode<SingleThreaded>,
-        cf_val: &Vec<(&str, &str)>,
-        hash: &str,
-        idx: usize,
-    ) {
-        let _: () = cf_val
-            .clone()
-            .into_iter()
-            .map(|(cf, val)| {
-                db.put_cf(db.cf_handle(cf).unwrap(), hash, val).unwrap();
-            })
-            .collect();
-
-        let _: () = cf_val
-            .into_iter()
-            .map(|(cf, _)| match db.get_cf(db.cf_handle(cf).unwrap(), hash) {
-                Ok(v) => println!(
-                    "key: {:?}, got value: {:?}",
-                    std::str::from_utf8(cf.as_bytes()),
-                    std::str::from_utf8(&v.unwrap())
-                ),
-                Err(_) => (),
-            })
-            .collect();
-    }
-
+    // #[tokio::test(flavor = "multi_thread")]
     fn batchput_and_get_transaction(
         db: &DBWithThreadMode<SingleThreaded>,
         cf_val: &Vec<(&str, &str)>,
@@ -136,6 +191,7 @@ mod test {
             .collect();
     }
 
+    // #[tokio::test(flavor = "multi_thread")]
     fn raw_iterator_to_first(
         db: &DBWithThreadMode<SingleThreaded>,
         cf_val: Vec<&str>,
@@ -155,33 +211,5 @@ mod test {
                 }
             })
             .collect();
-    }
-}
-#[cfg(test)]
-mod dummy {
-    pub(crate) struct Transaction<'a> {
-        pub tx_hash: &'a str,
-        pub pi: &'a str,
-        pub sig_vec: &'a str,
-        pub created_at: &'a str,
-        pub data: &'a str,
-    }
-
-    impl<'a> Transaction<'a> {
-        pub(crate) fn new(
-            tx_hash: &'a str,
-            pi: &'a str,
-            sig_vec: &'a str,
-            created_at: &'a str,
-            data: &'a str,
-        ) -> Transaction<'a> {
-            Transaction {
-                tx_hash,
-                pi,
-                sig_vec,
-                created_at,
-                data,
-            }
-        }
     }
 }
