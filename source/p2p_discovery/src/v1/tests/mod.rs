00eec288ff8b68d2cdfa2f94f4e44b67dbe42b65
c0np4nn4 <c0np4nn4@gmail.com>
2022-05-30 14:37:51 +0900

    rename the test address goes back in the queue to address has been registered.

--- source/p2p_discovery/src/v1/tests/mod.rs
+++ source/p2p_discovery/src/v1/tests/mod.rs
@@ -9,17 +9,18 @@ mod test {
     use super::utils;
     use crate::Discovery;
     use crate::DiscoveryArgs;
+    use tokio::time::Duration;
 
     fn init() {
         let _ = env_logger::builder().is_test(true).try_init();
     }
 
     #[tokio::test(flavor = "multi_thread")]
-    async fn test_addr_is_back_in_the_queue() {
+    async fn test_addr_has_been_registered_on_the_map() {
         init();
 
-        let disc_1 = utils::create_client_1().await;
-        let disc_2 = utils::create_client_2().await;
+        let disc_1 = utils::create_client(1).await;
+        let disc_2 = utils::create_client(2).await;
 
         let disc_1_clone = disc_1.clone();
         let disc_2_clone = disc_2.clone();
@@ -28,59 +29,59 @@ mod test {
             println!("running disc_1");
             disc_1_clone.run().await;
         });
+
         tokio::spawn(async move {
             println!("running disc_2");
             disc_2_clone.run().await;
         });
 
+        println!("Sleeping... 5sec");
+        tokio::time::sleep(Duration::from_secs(5)).await;
+
         let disc_1_clone = disc_1.clone();
+        let disc_2_clone = disc_2.clone();
 
-        let addr_iter_thread = tokio::spawn(async move {
-            let iter = disc_1_clone
-                .addr_table
-                .new_iter()
-                .expect("Addr iterator should be initialized");
+        let disc_1_table = disc_1_clone.addr_table.clone();
+        let disc_2_table = disc_2_clone.addr_table.clone();
 
-            let known_addr_ip: String;
-            let known_addr_disc_port: u16;
+        let disc_1_pub_key =
+            "04240874d8c323c22a571f735e835ed2f0619893a3989e557b1c9b4c699ac92b8\
+             4d0dc478108629c0353f2876941f90d4b36346bcc19c6b625422adffb53b3a6af";
 
-            {
-                let addr = iter.next().await.expect("Address should be popped");
-                let addr_lock = addr.read().await;
+        let disc_2_pub_key =
+            "04ce80d8c998044270b26eb7597bd92eb188807ace620644a34bf3be145422e61\
+             af51724079002c17758c33b88ade2e789a2153c1fd5b808c1f971127c2592009a";
 
-                // let addr = addr_guard.addr.write().await;
-                // let known_addr = match &addr.val {
-                //     AddrVal::Known(k) => k,
-                //     _ => panic!("Known addr should be provided"),
-                // };
-                let known_addr = &addr_lock.known_addr;
-
-                known_addr_ip = known_addr.ip.clone();
-                known_addr_disc_port = known_addr.disc_port.clone();
-
-                log::info!("Popped addr, {}", known_addr);
+        match disc_1_table
+            .get_mapped_addr(&disc_2_pub_key.to_string())
+            .await
+        {
+            Some(pub_key) => {
+                let pub_key = pub_key.read().await;
+                println!(
+                    "disc_1_mapped_addr: {}",
+                    pub_key.known_addr.disc_endpoint()
+                );
             }
+            None => {
+                panic!();
+            }
+        }
 
-            let addr = iter.next().await.expect("Address should be popped");
-            let addr_lock = addr.read().await;
-
-            // let addr = addr_guard.addr.write().await;
-            let known_addr = &addr_lock.known_addr;
-
-            log::info!("Popped addr, {}", known_addr);
-
-            log::info!(
-                "{:?} == {:?}",
-                (&known_addr_ip, known_addr_disc_port),
-                (known_addr.ip.clone(), known_addr.disc_port)
-            );
-
-            assert_eq!(
-                (known_addr_ip, known_addr_disc_port),
-                (known_addr.ip.clone(), known_addr.disc_port)
-            );
-        });
-
-        tokio::join!(addr_iter_thread);
+        match disc_2_table
+            .get_mapped_addr(&disc_1_pub_key.to_string())
+            .await
+        {
+            Some(pub_key) => {
+                let pub_key = pub_key.read().await;
+                println!(
+                    "disc_2_mapped_addr: {}",
+                    pub_key.known_addr.disc_endpoint()
+                );
+            }
+            None => {
+                panic!();
+            }
+        }
     }
 }
--- source/p2p_discovery/src/v1/tests/utils.rs
+++ source/p2p_discovery/src/v1/tests/utils.rs
@@ -3,104 +3,120 @@ use crate::DiscoveryArgs;
 use p2p_addr::AddrStatus;
 use p2p_addr::UnknownAddr;
 use p2p_identity::Credential;
+use std::ops::Index;
+use std::slice::SliceIndex;
 use std::sync::Arc;
 
 pub(super) fn init() {
     let _ = env_logger::builder().is_test(true).try_init();
 }
 
-pub(super) async fn create_client_1() -> Arc<Discovery> {
-    let secret = String::from(
-        "aa99cfd91cc6f3b541d28f3e0707f9c7bcf05cf495308294786ca450b501b5f2",
-    );
-
-    let public_key_str = String::from(
-        "\
-            04240874d8c323c22a571f735e835ed2\
-            f0619893a3989e557b1c9b4c699ac92b\
-            84d0dc478108629c0353f2876941f90d\
-            4b36346bcc19c6b625422adffb53b3a6af\
-            ",
-    );
-
-    let bootstrap_addrs = vec![UnknownAddr {
-        ip: String::from("127.0.0.1"),
-        disc_port: 35519,
-        p2p_port: None,
-        sig: None,
-        public_key_str: Some(String::from(
-            "\
-                04ce80d8c998044270b26eb7597bd92\
-                eb188807ace620644a34bf3be145422e\
-                61af51724079002c17758c33b88ade2e\
-                789a2153c1fd5b808c1f971127c2592009a\
-                ",
-        )),
-        status: AddrStatus::Initialized,
-    }];
-
-    let credential = {
-        let c = Credential::new(secret, public_key_str).unwrap();
-
-        Arc::new(c)
-    };
-
-    let disc_args = DiscoveryArgs {
-        disc_dial_interval: None,
-        disc_table_capacity: None,
-        disc_task_interval: None,
-        disc_task_queue_capacity: None,
-        credential: credential.clone(),
-        addr_expire_duration: None,
-        disc_port: Some(35520),
-        p2p_port: 1,
-        bootstrap_addrs,
-    };
-
-    let (p2p_discovery, disc_port) = {
-        let (disc, disc_port) = Discovery::init(disc_args)
-            .await
-            .expect("Discovery should be initailized");
-
-        (Arc::new(disc), disc_port)
-    };
-
-    p2p_discovery
+#[derive(Debug)]
+pub struct CliArgs {
+    pub secret_key_str: String,
+    pub public_key_str: String,
+    pub bootstrap_disc_port: u16,
+    pub bootstrap_public_key_str: Option<String>,
+    pub disc_args_disc_port: Option<u16>,
 }
 
-pub(super) async fn create_client_2() -> Arc<Discovery> {
-    let secret = String::from(
-        "445f36a1f7fafce4408b2277a5d009d1f1ba452d3996bfe7136de4adbfa34d61",
-    );
+fn get_bootstrap_addrs(
+    disc_port: u16,
+    public_key_str: Option<String>,
+) -> UnknownAddr {
+    UnknownAddr {
+        ip: String::from("127.0.0.1"),
+        disc_port,
+        p2p_port: None,
+        sig: None,
+        public_key_str,
+        status: AddrStatus::Initialized,
+    }
+}
 
-    let public_key_str = String::from(
-        "\
-            04ce80d8c998044270b26eb7597bd92e\
-            b188807ace620644a34bf3be145422e6\
-            1af51724079002c17758c33b88ade2e7\
-            89a2153c1fd5b808c1f971127c2592009a\
-            ",
-    );
-
-    let bootstrap_addrs = vec![];
-
-    let credential = {
-        let c = Credential::new(secret, public_key_str).unwrap();
-
-        Arc::new(c)
-    };
-
-    let disc_args = DiscoveryArgs {
+fn get_discovery_args(
+    secret_key_str: String,
+    public_key_str: String,
+    disc_port: Option<u16>,
+    bootstrap_addrs: Vec<UnknownAddr>,
+) -> DiscoveryArgs {
+    DiscoveryArgs {
         disc_dial_interval: None,
         disc_table_capacity: None,
         disc_task_interval: None,
         disc_task_queue_capacity: None,
         addr_expire_duration: None,
-        credential: credential.clone(),
-        disc_port: Some(35519),
-        p2p_port: 2,
+        credential: Arc::new(
+            Credential::new(secret_key_str, public_key_str).unwrap(),
+        ),
+        disc_port,
+        p2p_port: 1,
         bootstrap_addrs,
-    };
+    }
+}
+
+pub(super) async fn create_client(cli_index: u16) -> Arc<Discovery> {
+    let index: usize = cli_index as usize;
+
+    let test_clients = vec![ //
+        CliArgs {
+            secret_key_str: String::from("secret_key_str"),
+            public_key_str: String::from("public_key_str"),
+            bootstrap_disc_port: 65535,
+            bootstrap_public_key_str: Some(String::from("bootstrap_public_key_str")),
+            disc_args_disc_port: Some(65535),
+        },
+
+        // 1
+        CliArgs {
+        secret_key_str: String::from(
+            "aa99cfd91cc6f3b541d28f3e0707f9c7bcf05cf495308294786ca450b501b5f2",
+        ),
+        public_key_str: String::from(
+            "04240874d8c323c22a571f735e835ed2f0619893a3989e557b1c9b4c699ac92b8\
+             4d0dc478108629c0353f2876941f90d4b36346bcc19c6b625422adffb53b3a6af",
+        ),
+        bootstrap_disc_port: 35521,
+        bootstrap_public_key_str: Some(String::from(
+            "04240874d8c323c22a571f735e835ed2f0619893a3989e557b1c9b4c699ac92b8\
+             4d0dc478108629c0353f2876941f90d4b36346bcc19c6b625422adffb53b3a6af",
+        )),
+        disc_args_disc_port: Some(35521),
+    },
+        // 2
+        CliArgs {
+        secret_key_str: String::from(
+            "445f36a1f7fafce4408b2277a5d009d1f1ba452d3996bfe7136de4adbfa34d61",
+        ),
+        public_key_str: String::from(
+            "04ce80d8c998044270b26eb7597bd92eb188807ace620644a34bf3be145422e61\
+             af51724079002c17758c33b88ade2e789a2153c1fd5b808c1f971127c2592009a",
+        ),
+        bootstrap_disc_port: 35521,
+        bootstrap_public_key_str: Some(String::from(
+            "04240874d8c323c22a571f735e835ed2f0619893a3989e557b1c9b4c699ac92b8\
+             4d0dc478108629c0353f2876941f90d4b36346bcc19c6b625422adffb53b3a6af",
+        )),
+        disc_args_disc_port: Some(35522),
+    },
+
+
+    ];
+
+    let secret_key_str = test_clients[index].secret_key_str.clone();
+    let public_key_str = test_clients[index].public_key_str.clone();
+
+    let bootstrap_addrs = vec![get_bootstrap_addrs(
+        test_clients[index].bootstrap_disc_port.clone(),
+        test_clients[index].bootstrap_public_key_str.clone(),
+    )];
+
+    let disc_args = get_discovery_args(
+        secret_key_str,
+        public_key_str,
+        test_clients[index].disc_args_disc_port.clone(),
+        bootstrap_addrs,
+    );
 
     let (p2p_discovery, disc_port) = {
         let (disc, disc_port) = Discovery::init(disc_args)
