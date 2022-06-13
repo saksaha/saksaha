b9bf880b79eba5ff010126fdc836b8d6af33fc13
jinwook jang <jinwook.jang@saksaha.com>
2022-06-13 18:53:46 +0900

    bchain: add timeout for tx pool sync
    * bchain: add sync_task_queue
    
    * bchain:add event queue to sync tx pool
    
    * bchain: write_block
    
    * refactor mpsc to broadcast
    
    * clean unused log
    
    Co-authored-by: Elden Park <elden@saksaha.com>

--- source/saksaha/src/node/event_handle.rs
+++ source/saksaha/src/node/event_handle.rs
@@ -1,27 +1,27 @@
+use crate::machine::Machine;
 use futures::{SinkExt, StreamExt};
 use log::{info, warn};
-use sak_p2p_trpt::{Connection, Msg, TxHashSyn, TxSyn};
+use sak_p2p_trpt::{Connection, Msg, TxHashSync, TxSyn};
+use std::time::Duration;
 use tokio::sync::RwLockWriteGuard;
 
-use crate::machine::Machine;
+const RESPONSE_TIMEOUT: u64 = 2000;
 
 pub(super) async fn handle_tx_pool_stat<'a>(
+    public_key: &str,
     conn: &'a mut RwLockWriteGuard<'_, Connection>,
     machine: &Machine,
     new_tx_hashes: Vec<String>,
 ) {
     match conn
         .socket
-        .send(Msg::TxHashSyn(TxHashSyn {
+        .send(Msg::TxHashSyn(TxHashSync {
             tx_hashes: new_tx_hashes,
         }))
         .await
     {
         Ok(_) => {
-            info!(
-                "Incoming tx successfully requested to sync with \
-                the peer node, send msg type: TxHashSyn"
-            );
+            info!("Sending TxHashSyn, public_key: {}", public_key);
         }
         Err(err) => {
             warn!(
@@ -31,40 +31,47 @@ pub(super) async fn handle_tx_pool_stat<'a>(
         }
     };
 
-    let txs = match conn.socket.next().await {
-        Some(maybe_msg) => match maybe_msg {
-            Ok(msg) => match msg {
-                Msg::TxHashAck(h) => {
-                    let txs = machine
-                        .blockchain
-                        .get_ack_txs_from_pool(h.tx_hashes)
-                        .await;
+    let resp_timeout =
+        tokio::time::sleep(Duration::from_millis(RESPONSE_TIMEOUT));
 
-                    txs
-                }
-                _ => {
-                    warn!("Received an invalid type message");
+    let txs = tokio::select! {
+        _ = resp_timeout => {
+            warn!("Peer did not respond in time, public_key: {}", public_key);
+            return;
+        },
+        resp = conn.socket.next() => {
+            match resp {
+                Some(maybe_msg) => match maybe_msg {
+                    Ok(msg) => match msg {
+                        Msg::TxHashAck(h) => {
+                            let txs = machine
+                                .blockchain
+                                .get_ack_txs_from_pool(h.tx_hashes)
+                                .await;
+
+                            txs
+                        }
+                        _ => {
+                            warn!("Received an invalid type message");
+                            return;
+                        }
+                    },
+                    Err(err) => {
+                        warn!("Failed to parse the msg, err: {}", err);
+                        return;
+                    }
+                },
+                None => {
+                    warn!("Received an invalid data stream");
                     return;
                 }
-            },
-            Err(err) => {
-                warn!("Failed to parse the msg, err: {}", err);
-                return;
             }
         },
-        None => {
-            warn!("Received an invalid data stream");
-            return;
-        }
     };
 
     match conn.socket.send(Msg::TxSyn(TxSyn { txs })).await {
         Ok(_) => {
-            info!(
-                "requested transactions are successfully \
-                transmitted to the peer node, send msg \
-                type: TxHSyn"
-            );
+            info!("Sending TxSyn, public_key: {}", public_key);
         }
         Err(err) => {
             info!("Failed to send requested tx, err: {}", err,);
