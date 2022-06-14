ebe10635cef213d4454c74906736e26edac75c52
Elden Park <eldeniyenden@gmail.com>
2022-06-14 15:28:58 +0900

    hotfix(vm): seperate 'test_validator' portion from 'vm.rs'

--- source/sak_vm/src/v0/mod.rs
+++ source/sak_vm/src/v0/mod.rs
@@ -1,10 +1,12 @@
 mod constants;
 mod storage;
+mod test_validator;
 mod utils;
 mod vm;
 
 pub(crate) use constants::*;
 pub use storage::*;
+pub(crate) use test_validator::*;
 pub use vm::*;
 
 pub(crate) type BoxedError = Box<dyn std::error::Error + Send + Sync>;
