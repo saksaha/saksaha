a304466db97f2c9036a11d62db89d7a2ec23fd81
Kihyun Kim <kihyun@saksaha.com>
2022-06-29 19:12:05 +0900

    add init test configs
    changed rocks_db to sak_kv_db and fixed a minor issue
    
    modified the init test config reflecting the review
    
    changed minor issues

---
+++ source/saksaha/src/blockchain/tests/utils.rs
@@ -0,0 +1,27 @@
+#[cfg(test)]
+pub(crate) mod test_utils {
+    use log::info;
+    use sak_fs::FS;
+    use sak_kv_db::{Options, DB};
+    pub fn test_config_init(app_prefix_vec: Vec<String>) -> Result<(), String> {
+        for app_prefix in app_prefix_vec {
+            let db_path = FS::create_or_get_app_path(&app_prefix)
+                .expect("Failed to create or get the db_path");
+            let ledger_path = db_path.join("db").join("ledger");
+
+            if !ledger_path.is_dir() {
+                continue;
+            }
+
+            let _ = match DB::destroy(&Options::default(), ledger_path.clone())
+            {
+                Ok(_) => (),
+                Err(err) => return Err(err.to_string()),
+            };
+        }
+
+        info!("Initialized test configurations");
+
+        Ok(())
+    }
+}
