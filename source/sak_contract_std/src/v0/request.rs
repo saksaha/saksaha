63a11efd313a3f7d90669255f93463eb57588ac5
c0np4nn4 <c0np4nn4@gmail.com>
2022-07-04 11:40:28 +0900

    dledger: add a field `ctr_call_type` in struct `Request`
    - add a field `ctr_call_type` in struct `Request`
    
    - sak_dist_ledger: state overwritten issue has been found
    
    - sak_dist_ledger: updates state with `previous_state`
    
    Co-authored-by: Elden Park <elden@saksaha.com>

--- source/sak_contract_std/src/v0/request.rs
+++ source/sak_contract_std/src/v0/request.rs
@@ -2,15 +2,22 @@ use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 
 #[derive(Serialize, Deserialize, Debug)]
+pub enum CtrCallType {
+    Query,
+    Execute,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
 pub struct Request {
     pub req_type: String,
     pub arg: HashMap<String, String>,
+    pub ctr_call_type: CtrCallType,
 }
 
 impl Request {
     pub fn parse(data: &[u8]) -> Result<Request, String> {
         match serde_json::from_slice(data) {
-            Ok(o) => o,
+            Ok(o) => Ok(o),
             Err(err) => {
                 return Err(format!("Error parsing request, err: {}", err));
             }
