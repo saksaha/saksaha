248a818912dc0377c40cb6720d899f98e5fb5263
Elden Park <elden@saksaha.com>
2022-07-05 13:34:15 +0900

    o

--- source/sak_proofs/src/v0/merkle.rs
+++ source/sak_proofs/src/v0/merkle.rs
@@ -1,11 +1,8 @@
+use crate::MiMC;
 use bls12_381::Scalar;
-use ff::Field;
-use ff::PrimeField;
-use rand::thread_rng;
-use sha2::{Digest, Sha256};
 
 #[derive(Debug)]
-pub struct Tree {
+pub struct MerkleTree {
     pub nodes: Vec<Vec<Node>>,
     pub height: usize,
     pub data: Vec<u32>,
@@ -23,20 +20,12 @@ pub struct Path {
     pub hash: Scalar,
 }
 
-const fn num_bits<T>() -> usize {
-    std::mem::size_of::<T>() * 8
-}
-
 fn copy_node(node: &Node) -> Node {
     Node {
         val: node.val,
 
         hash: node.hash.clone(),
     }
-}
-
-fn log_floor(num: usize) -> u32 {
-    num_bits::<u64>() as u32 - num.leading_zeros() - 1
 }
 
 fn get_sibling_idx(idx: u64) -> u64 {
@@ -51,24 +40,12 @@ fn get_parent_idx(idx: u64) -> u64 {
     idx / 2
 }
 
-pub fn mimc<S: PrimeField>(mut xl: S, mut xr: S, constants: &[S]) -> S {
-    // assert_eq!(constants.len(), MIMC_ROUNDS);
-
-    for c in constants {
-        let mut tmp1 = xl;
-        tmp1.add_assign(c);
-        let mut tmp2 = tmp1.square();
-        tmp2.mul_assign(&tmp1);
-        tmp2.add_assign(&xr);
-        xr = xl;
-        xl = tmp2;
-    }
-
-    xl
-}
-
-impl Tree {
-    pub fn new(data: Vec<u32>, height: usize, constants: &[Scalar]) -> Tree {
+impl MerkleTree {
+    pub fn new(
+        data: Vec<u32>,
+        height: usize,
+        constants: &[Scalar],
+    ) -> MerkleTree {
         let mut leaves = vec![];
         let leaf_count = data.len();
 
@@ -82,7 +59,8 @@ impl Tree {
         for l in data.into_iter() {
             let xl: u64 = l.into();
             let xr: u64 = (l + 1).into();
-            let hash = mimc(Scalar::from(xl), Scalar::from(xr), constants);
+            let hash =
+                MiMC::mimc(Scalar::from(xl), Scalar::from(xr), constants);
 
             let n = Node {
                 val: Some([l]),
@@ -105,25 +83,13 @@ impl Tree {
             }
 
             let mut xl = Scalar::default();
-            // let mut xr = Scalar::default();
 
-            // let mut combined: [u8; 64] = [0; 64];
             for (idx, cn) in child_nodes.iter().enumerate() {
                 if idx % 2 == 0 {
-                    // combined[..32].clone_from_slice(&cn.hash);
-                    // hasher.update(cn.hash.to_owned());
                     xl = cn.hash;
                 } else {
-                    // combined[32..].clone_from_slice(&cn.hash);
-                    // println!("combining: {:?}", combined);
-
-                    // let hs = Sha256::digest(&combined);
                     let xr = cn.hash;
-                    let hs = mimc(xl, xr, &constants);
-                    // let mut hash: [u8; 32] = Default::default();
-                    // hash.copy_from_slice(&hs[..]);
-
-                    // println!("height: {}, hash: {:?}", h, hs);
+                    let hs = MiMC::mimc(xl, xr, &constants);
 
                     let n = Node {
                         val: None,
@@ -131,7 +97,6 @@ impl Tree {
                     };
 
                     nodes_at_height.push(n);
-                    // combined = [0; 64];
                 }
             }
             nodes.push(nodes_at_height);
@@ -146,7 +111,7 @@ impl Tree {
             );
         }
 
-        Tree {
+        MerkleTree {
             nodes,
             height,
             data: d,
---
+++ source/sak_proofs/src/v0/proofs/mod.rs
@@ -0,0 +1,2 @@
+pub mod coin_ownership;
+
