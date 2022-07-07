950b53da15ed5303d3b8b5309737b196d7fa4e8e
Elden Park <elden@saksaha.com>
2022-07-07 18:03:23 +0900

    o

---
+++ source/sak_vm/src/v0/tests/test_msg_multi_clients.rs
@@ -0,0 +1,36 @@
+#[tokio::test(flavor = "multi_thread")]
+async fn test_multi_clients_chat() {
+    // init();
+
+    // let cli_1;
+    // let cli_2;
+
+    let pubkey_1 = {};
+
+    let pubkey_2 = {};
+
+    let shared_secret = {
+        // rand gen
+
+        // aes
+        // key
+    };
+
+    {
+        // let msg1 = "blabla";
+        // cipher <= fn(key, ..);
+        // cipher.encrypt()
+        // send msg
+        1
+    };
+
+    {
+        // cipher <= fn(key, ..);
+        // let msg2 = cipher.decrypt()
+
+        // read msg
+        2
+    };
+
+    // check if msg1 == msg2
+}
