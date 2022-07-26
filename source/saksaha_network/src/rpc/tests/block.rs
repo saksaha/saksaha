8d710e69e36ac37cbbdd5738c1296d3d4e850a6d
c0np4nn4 <c0np4nn4@gmail.com>
2022-07-26 21:53:36 +0900

    sak/rpc: add a tc, calling get_block() w/ wrong params

--- source/saksaha_network/src/rpc/tests/block.rs
+++ source/saksaha_network/src/rpc/tests/block.rs
@@ -1,10 +1,10 @@
 use super::utils;
 use crate::rpc::router::{JsonRequest, JsonResponse};
-use crate::rpc::routes::v0::{GetBlockRequest, GetBlockResponse};
+use crate::rpc::routes::v0:: GetBlockResponse;
 use hyper::{Body, Client, Method, Request, Uri};
 
 #[tokio::test(flavor = "multi_thread")]
-async fn test_rpc_client_and_get_block() {
+async fn test_call_get_block_with_good_params() {
     sak_test_utils::init_test_log();
     sak_test_utils::init_test_config(&vec![String::from("test")]).unwrap();
 
@@ -41,11 +41,12 @@ async fn test_rpc_client_and_get_block() {
     };
 
     let body = {
-        let r = GetBlockRequest {
-            block_hash: original_block_hash.to_string(),
-        };
-
-        let params = serde_json::to_string(&r).unwrap().as_bytes().to_vec();
+        let params = 
+r#"
+{
+    "block_hash": "973f486c42f67e8520367a46f1a13caf969224d99d1b2f02943c6d926b7bc04b"
+}
+"#.as_bytes().to_vec();
 
         let json_request = JsonRequest {
             jsonrpc: "2.0".to_string(),
@@ -55,6 +56,8 @@ async fn test_rpc_client_and_get_block() {
         };
 
         let str = serde_json::to_string(&json_request).unwrap();
+
+        println!("[+] request body str (for debugging): {:#?}", str);
 
         Body::from(str)
     };
@@ -76,10 +79,95 @@ async fn test_rpc_client_and_get_block() {
 
     let block_acquired = result.block.unwrap();
 
+    assert_eq!(block_acquired.get_block_hash(), &original_block_hash);
+}
+
+#[tokio::test(flavor = "multi_thread")]
+#[should_panic]
+async fn test_call_get_block_with_wrong_params() {
+    sak_test_utils::init_test_log();
+    sak_test_utils::init_test_config(&vec![String::from("test")]).unwrap();
+
+    let (rpc, rpc_socket_addr, machine) = utils::make_test_context().await;
+
+    tokio::spawn(async move { rpc.run().await });
+
+    let client = Client::new();
+
+    let block_candidate = utils::make_dummy_tx_pour_block();
+
+    let original_block_hash = {
+        let block_hash = match machine
+            .blockchain
+            .dist_ledger
+            .apis
+            .write_block(Some(block_candidate))
+            .await
+        {
+            Ok(v) => v,
+            Err(err) => panic!("Failed to write dummy block, err: {}", err),
+        };
+
+        block_hash.unwrap()
+    };
+
+    let uri: Uri = {
+        let u = format!("http://localhost:{}", rpc_socket_addr.port());
+
+        u.parse().expect("URI should be made")
+    };
+
+    let body = {
+        // original_block_hash == 973f486c42f67e8520367a46f1a13caf969224d99d1b2f02943c6d926b7bc04b
+        let params = 
+r#"
+973f486c42f67e8520367a46f1a13caf969224d99d1b2f02943c6d926b7bc04b
+"#.as_bytes().to_vec();
+
+        let json_request = JsonRequest {
+            jsonrpc: "2.0".to_string(),
+            method: "get_block".to_string(),
+            params: Some(params),
+            id: "test_1".to_string(),
+        };
+
+        let str = serde_json::to_string(&json_request).unwrap();
+
+        println!("[+] request body str (for debugging): {:#?}", str);
+
+        Body::from(str)
+    };
+
+    let req = Request::builder()
+        .method(Method::POST)
+        .uri(uri)
+        .body(body)
+        .expect("request builder should be made");
+
+    let resp = client.request(req).await.unwrap();
+
+    let b = hyper::body::to_bytes(resp.into_body()).await.unwrap();
+
+    let resp_str = std::str::from_utf8(&b).unwrap();
+
+    println!("[+] response (for debugging): {}", resp_str);
+
+    let json_response =
+        serde_json::from_slice::<JsonResponse<GetBlockResponse>>(&b).unwrap();
+
+    println!("[+] resp struct: {:?}", json_response);
+
+    let result = json_response.result.unwrap();
+
+    let block_acquired = result.block.unwrap();
+
     println!(
-        "block hash (from rpc response) : {:?}",
+        "[+] block hash (from rpc response) : {:?}",
         block_acquired.get_block_hash(),
     );
 
     assert_eq!(block_acquired.get_block_hash(), &original_block_hash);
+
+    //
 }
+
