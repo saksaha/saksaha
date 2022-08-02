d14492e5a961b7c0d6f84244798bb39a371ba9ed
Kihyun Kim <kihyun@saksaha.com>
2022-08-02 18:48:18 +0900

    refactor and fix the envelope contract of test cases

--- extern/envelope_contract/tests/default/constants.rs
+++ extern/envelope_contract/tests/default/constants.rs
@@ -1,17 +1,24 @@
 pub const STORAGE_CAP: usize = 100;
 
+pub const INIT_CHANNEL_ID_1: &str = "ch_12";
+
 pub const DUMMY_CHANNEL_ID_1: &str = "dummy_channel_1";
 
 pub const DUMMY_CHANNEL_ID_2: &str = "dummy_channel_2";
+
+pub const DUMMY_CHANNEL_ID_3: &str = "dummy_channel_3";
 
 pub const ENVELOPE_CONTRACT: &[u8] = include_bytes!(
     "../../../../source/prebuild/\
         envelope_contract.postprocess.wasm"
 );
 
-pub(crate) const ARG_CH_ID: &str = "ch_id";
-pub(crate) const ARG_CIPHER_TEXT: &str = "cipher_text";
-pub(crate) const ARG_EPH_PK: &str = "eph_pk";
-pub(crate) const ARG_SRC_PK: &str = "src_pk";
-pub(crate) const ARG_DST_PK: &str = "dst_pk";
-pub(crate) const ARG_SERIALIZED_INPUT: &str = "serialized_input";
+pub const ARG_CH_ID: &str = "ch_id";
+
+pub const ARG_DST_PK: &str = "dst_pk";
+
+pub const ARG_SERIALIZED_INPUT: &str = "serialized_input";
+
+// pub const ARG_CIPHER_TEXT: &str = "cipher_text";
+// pub const ARG_EPH_PK: &str = "eph_pk";
+// pub const ARG_SRC_PK: &str = "src_pk";
--- extern/envelope_contract/tests/default/test_messenger.rs
+++ extern/envelope_contract/tests/default/test_messenger.rs
@@ -1,10 +1,18 @@
 use super::{
-    ARG_DST_PK, DUMMY_CHANNEL_ID_1, DUMMY_CHANNEL_ID_2, ENVELOPE_CONTRACT,
-    STORAGE_CAP,
+    ARG_CH_ID, ARG_DST_PK, ARG_SERIALIZED_INPUT, DUMMY_CHANNEL_ID_1,
+    DUMMY_CHANNEL_ID_2, DUMMY_CHANNEL_ID_3, ENVELOPE_CONTRACT,
+    INIT_CHANNEL_ID_1, STORAGE_CAP,
 };
 use sak_contract_std::{CtrCallType, Request, Storage};
 use sak_vm::{CtrFn, VM};
 use std::collections::HashMap;
+
+pub(crate) struct OpenChInput {
+    eph_pk: String,
+    ch_id: String,
+    sign: String,
+    chat: String,
+}
 
 fn get_single_message() -> String {
     String::from("Hello! I belong to saksaha")
@@ -30,13 +38,33 @@ fn make_dummy_storage(msgs: &Vec<String>) -> Storage {
 
     ret.insert(key, value);
 
+    let key = String::from(get_her_pk());
+
+    let input: Vec<String> = vec![
+        String::default(),
+        DUMMY_CHANNEL_ID_1.to_string(),
+        String::default(),
+        String::default(),
+    ];
+
+    let input = serde_json::to_string(&input).unwrap();
+
+    let input_vec: Vec<String> = vec![input];
+
+    let value = serde_json::to_string(&input_vec).unwrap();
+
+    ret.insert(key, value);
+
     ret
 }
 
-fn make_empty_storage() -> Storage {
-    let ret = Storage::with_capacity(STORAGE_CAP);
-
-    ret
+fn make_dummy_open_ch_input() -> OpenChInput {
+    OpenChInput {
+        eph_pk: String::default(),
+        ch_id: DUMMY_CHANNEL_ID_2.to_string(),
+        sign: String::default(),
+        chat: String::default(),
+    }
 }
 
 #[tokio::test(flavor = "multi_thread")]
@@ -55,7 +83,7 @@ async fn test_messenger_init() {
         serde_json::from_str(messenger_states_invoked.as_str()).unwrap();
 
     let messages: Vec<String> =
-        serde_json::from_str(messenger_states.get(DUMMY_CHANNEL_ID_1).unwrap())
+        serde_json::from_str(messenger_states.get(INIT_CHANNEL_ID_1).unwrap())
             .unwrap();
 
     let messages_expected = get_multi_messages();
@@ -76,10 +104,7 @@ async fn test_messenger_get_msgs() {
 
     let request = {
         let mut arg = HashMap::with_capacity(1);
-        arg.insert(
-            String::from("channel_id"),
-            String::from(DUMMY_CHANNEL_ID_1),
-        );
+        arg.insert(String::from(ARG_CH_ID), String::from(DUMMY_CHANNEL_ID_1));
 
         Request {
             req_type: "get_msgs".to_string(),
@@ -88,20 +113,22 @@ async fn test_messenger_get_msgs() {
         }
     };
 
-    let ctr_wasm = ENVELOPE_CONTRACT.to_vec();
-    let ctr_fn = CtrFn::Query(request, messages_state);
+    {
+        let ctr_wasm = ENVELOPE_CONTRACT.to_vec();
+        let ctr_fn = CtrFn::Query(request, messages_state);
 
-    let messages_from_query = vm
-        .invoke(ctr_wasm, ctr_fn)
-        .expect("message should be obtained");
+        let messages_from_query = vm
+            .invoke(ctr_wasm, ctr_fn)
+            .expect("message should be obtained");
 
-    let messages: Vec<String> =
-        serde_json::from_str(messages_from_query.as_str()).unwrap();
+        let messages: Vec<String> =
+            serde_json::from_str(messages_from_query.as_str()).unwrap();
 
-    println!("messages expected: {:?}", test_dummy_messege);
-    println!("messages acquired: {:?}", messages);
+        println!("messages expected: {:?}", test_dummy_messege);
+        println!("messages acquired: {:?}", messages);
 
-    assert_eq!(test_dummy_messege, messages);
+        assert_eq!(test_dummy_messege, messages);
+    }
 }
 
 #[tokio::test(flavor = "multi_thread")]
@@ -119,49 +146,58 @@ async fn test_messenger_get_ch_list() {
         let req = Request {
             req_type: String::from("get_ch_list"),
             arg,
-            ctr_call_type: CtrCallType::Execute,
+            ctr_call_type: CtrCallType::Query,
         };
         let storage = make_dummy_storage(&dummy_messeges);
         (req, storage)
     };
 
-    let ctr_wasm = ENVELOPE_CONTRACT.to_vec();
-    let ctr_fn = CtrFn::Query(request, storage);
+    {
+        let ctr_wasm = ENVELOPE_CONTRACT.to_vec();
+        let ctr_fn = CtrFn::Query(request, storage);
 
-    let ch_list_serialized = match vm.invoke(ctr_wasm, ctr_fn) {
-        Ok(s) => s,
-        Err(err) => panic!("failed to invoke contract : {}", err),
-    };
+        let ch_list_serialized = match vm.invoke(ctr_wasm, ctr_fn) {
+            Ok(s) => s,
+            Err(err) => panic!("failed to invoke contract : {}", err),
+        };
 
-    let open_ch_data_vec: Vec<Vec<String>> =
-        serde_json::from_str(&ch_list_serialized).unwrap();
+        let open_ch_data_vec: Vec<String> =
+            serde_json::from_str(&ch_list_serialized).unwrap();
 
-    let mut ch_list = vec![];
-    for data in open_ch_data_vec {
-        ch_list.push(data[1].clone());
+        println!("expected channel id : {:?}", vec![DUMMY_CHANNEL_ID_1]);
+        println!("updated channel id: {:?}", open_ch_data_vec);
+
+        assert_eq!(vec![DUMMY_CHANNEL_ID_1], open_ch_data_vec);
     }
-
-    println!("expected channel id : {:?}", DUMMY_CHANNEL_ID_2);
-    println!("updated channel id: {:?}", ch_list);
-
-    assert_eq!(vec![DUMMY_CHANNEL_ID_2], ch_list);
 }
 
 #[tokio::test(flavor = "multi_thread")]
 async fn test_messenger_open_channel() {
     let vm = VM::init().expect("VM should be initiated");
 
-    let her_pk = get_her_pk();
+    let new_pk = "abcdef".to_string();
 
     let dummy_messeges = get_multi_messages();
 
+    let OpenChInput {
+        eph_pk,
+        ch_id,
+        sign,
+        chat,
+    } = make_dummy_open_ch_input();
+
+    let input = {
+        let input = vec![eph_pk, ch_id, sign, chat];
+
+        let input_str = serde_json::to_string(&input).unwrap();
+
+        input_str
+    };
+
     let (request, storage) = {
         let mut arg = HashMap::with_capacity(2);
-        arg.insert(String::from("her_pk"), her_pk.clone());
-        arg.insert(
-            String::from("channel_id"),
-            String::from(DUMMY_CHANNEL_ID_2),
-        );
+        arg.insert(String::from(ARG_DST_PK), new_pk.clone());
+        arg.insert(String::from(ARG_SERIALIZED_INPUT), input);
 
         let req = Request {
             req_type: String::from("open_channel"),
@@ -172,38 +208,46 @@ async fn test_messenger_open_channel() {
         (req, storage)
     };
 
-    let ctr_wasm = ENVELOPE_CONTRACT.to_vec();
-    let ctr_fn = CtrFn::Execute(request, storage);
+    {
+        let ctr_wasm = ENVELOPE_CONTRACT.to_vec();
+        let ctr_fn = CtrFn::Execute(request, storage);
 
-    let state_serialized = match vm.invoke(ctr_wasm, ctr_fn) {
-        Ok(s) => s,
-        Err(err) => panic!("faeild to invoke contract : {}", err),
-    };
+        let state_serialized = match vm.invoke(ctr_wasm, ctr_fn) {
+            Ok(s) => s,
+            Err(err) => panic!("faeild to invoke contract : {}", err),
+        };
 
-    let chats_state: Storage =
-        serde_json::from_str(state_serialized.as_str()).unwrap();
+        let chats_state: Storage =
+            serde_json::from_str(state_serialized.as_str()).unwrap();
 
-    let channel_id = chats_state.get(&her_pk).unwrap();
+        let open_ch_serialized = chats_state.get(&new_pk).unwrap();
+        let open_ch_vec: Vec<String> =
+            serde_json::from_str(open_ch_serialized).unwrap();
 
-    println!("expected channel id : {:?}", DUMMY_CHANNEL_ID_2);
-    println!("updated channel id: {:?}", channel_id);
+        let open_ch: Vec<String> =
+            serde_json::from_str(&open_ch_vec[0]).unwrap();
 
-    assert_eq!(DUMMY_CHANNEL_ID_2, channel_id);
+        println!("expected channel id : {:?}", DUMMY_CHANNEL_ID_2);
+        println!("updated channel id: {:?}", open_ch);
+
+        let ch_id = &open_ch[1];
+
+        assert_eq!(DUMMY_CHANNEL_ID_2, ch_id);
+    }
 }
 
 #[tokio::test(flavor = "multi_thread")]
 async fn test_messenger_send_msg() {
     let vm = VM::init().expect("VM should be initiated");
 
-    let mut dummy_messeges = get_multi_messages();
+    let dummy_messeges = get_multi_messages();
+
+    let expected_msg = get_single_message();
 
     let (request, storage) = {
         let mut arg = HashMap::with_capacity(2);
-        arg.insert(
-            String::from("channel_id"),
-            String::from(DUMMY_CHANNEL_ID_1),
-        );
-        arg.insert(String::from("message"), get_single_message());
+        arg.insert(String::from(ARG_CH_ID), String::from(DUMMY_CHANNEL_ID_3));
+        arg.insert(String::from(ARG_SERIALIZED_INPUT), expected_msg.clone());
 
         let req = Request {
             req_type: String::from("send_msg"),
@@ -214,25 +258,21 @@ async fn test_messenger_send_msg() {
         (req, storage)
     };
 
-    let ctr_wasm = ENVELOPE_CONTRACT.to_vec();
-    let ctr_fn = CtrFn::Execute(request, storage);
+    {
+        let ctr_wasm = ENVELOPE_CONTRACT.to_vec();
+        let ctr_fn = CtrFn::Execute(request, storage);
 
-    let chats_state_serialized = vm
-        .invoke(ctr_wasm, ctr_fn)
-        .expect("State should be obtained");
+        let chats_state_serialized = vm
+            .invoke(ctr_wasm, ctr_fn)
+            .expect("State should be obtained");
 
-    let chats_state: Storage =
-        serde_json::from_str(chats_state_serialized.as_str()).unwrap();
+        let chats_state: Storage =
+            serde_json::from_str(chats_state_serialized.as_str()).unwrap();
 
-    let msgs_serialized = chats_state.get(DUMMY_CHANNEL_ID_1).unwrap();
+        let msg = chats_state.get(DUMMY_CHANNEL_ID_3).unwrap();
+        println!("expected msgs: {:?}", expected_msg);
+        println!("updated msgs: {:?}", msg);
 
-    let msgs: Vec<String> =
-        serde_json::from_str(msgs_serialized.as_str()).unwrap();
-
-    dummy_messeges.push(get_single_message());
-
-    println!("expected msgs: {:?}", dummy_messeges);
-    println!("updated msgs: {:?}", msgs);
-
-    assert_eq!(dummy_messeges, msgs);
+        assert_eq!(&expected_msg, msg);
+    };
 }
