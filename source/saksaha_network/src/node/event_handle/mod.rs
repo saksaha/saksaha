8ea4318d7c342ffe35818895b7bb6529e8f227e0
Jinwook Noah Jang <noah@saksaha.com>
2022-08-03 19:17:29 +0900

    wip

--- source/saksaha_network/src/node/event_handle/mod.rs
+++ source/saksaha_network/src/node/event_handle/mod.rs
@@ -1,19 +1,17 @@
-use crate::machine::Machine;
-use futures::SinkExt;
+use crate::{machine::Machine, SaksahaError};
+use futures::{executor::block_on, stream::SplitSink, SinkExt};
 use log::{info, warn};
-use sak_p2p_transport::{
-    BlockHashSynMsg, Msg, TxHashSynMsg, UpgradedConnection,
-};
-use tokio::sync::RwLockWriteGuard;
+use sak_p2p_transport::{BlockHashSynMsg, Msg, TxHashSynMsg, UpgradedP2PCodec};
+use tokio::net::TcpStream;
+use tokio_util::codec::Framed;
 
 pub(super) async fn handle_tx_pool_stat<'a>(
     public_key: &str,
-    conn: &'a mut RwLockWriteGuard<'_, UpgradedConnection>,
+    socket_tx: &mut SplitSink<Framed<TcpStream, UpgradedP2PCodec>, Msg>,
     _machine: &Machine,
     new_tx_hashes: Vec<String>,
 ) {
-    match conn
-        .socket
+    match socket_tx
         .send(Msg::TxHashSyn(TxHashSynMsg {
             tx_hashes: new_tx_hashes,
         }))
@@ -33,15 +31,43 @@ pub(super) async fn handle_tx_pool_stat<'a>(
 
 pub(super) async fn handle_new_blocks_ev<'a>(
     public_key: &str,
-    conn: &'a mut RwLockWriteGuard<'_, UpgradedConnection>,
+    socket_tx: &mut SplitSink<Framed<TcpStream, UpgradedP2PCodec>, Msg>,
     _machine: &Machine,
     new_blocks: Vec<(u128, String)>,
 ) {
-    match conn
-        .socket
+    match socket_tx
         .send(Msg::BlockHashSyn(BlockHashSynMsg {
             new_blocks: new_blocks.clone(),
         }))
+        .await
+    {
+        Ok(_) => {
+            info!("Sending block hash syn, dst public_key: {}", public_key);
+        }
+        Err(err) => {
+            warn!(
+                "Failed to request to synchronize with peer node, err: {}",
+                err,
+            );
+        }
+    };
+}
+
+pub(super) async fn handle_new_peers_ev<'a>(
+    public_key: &str,
+    socket_tx: &mut SplitSink<Framed<TcpStream, UpgradedP2PCodec>, Msg>,
+    machine: &Machine,
+) {
+    let blocks = machine
+        .blockchain
+        .dist_ledger
+        .apis
+        .get_entire_block_info_list()
+        .await
+        .unwrap_or(vec![]);
+
+    match socket_tx
+        .send(Msg::BlockHashSyn(BlockHashSynMsg { new_blocks: blocks }))
         .await
     {
         Ok(_) => {
