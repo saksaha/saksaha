81475c16bec83c57eee5039db82d3ea2abce8d93
Elden Park <elden@saksaha.com>
2022-08-03 18:53:01 +0900

    o

--- source/saksaha_wallet/src/rpc/rpc.rs
+++ source/saksaha_wallet/src/rpc/rpc.rs
@@ -1,19 +1,42 @@
+use crate::WalletError;
+use colored::Colorize;
+use log::{error, info};
 use std::time::Duration;
-
-const RPC_PORT: u16 = 37701;
+use tokio::net::TcpListener;
 
 pub(crate) struct RPC {
     rpc_port: u16,
+    rpc_socket: TcpListener,
 }
 
 impl RPC {
-    pub fn init(rpc_port: Option<u16>) -> RPC {
-        let rpc_port = rpc_port.unwrap_or(RPC_PORT);
+    pub async fn init(rpc_port: Option<u16>) -> Result<RPC, WalletError> {
+        let (rpc_socket, socket_addr) =
+            match sak_utils_net::bind_tcp_socket(rpc_port).await {
+                Ok((socket, socket_addr)) => {
+                    info!(
+                        "Bound tcp socket for RPC, addr: {}",
+                        socket_addr.to_string().yellow(),
+                    );
 
-        RPC { rpc_port }
+                    (socket, socket_addr)
+                }
+                Err(err) => {
+                    error!("Could not bind a tcp socket for RPC, err: {}", err);
+
+                    return Err(err.into());
+                }
+            };
+
+        let rpc = RPC {
+            rpc_port: socket_addr.port(),
+            rpc_socket,
+        };
+
+        Ok(rpc)
     }
 
-    pub async fn run(&self) {
+    pub async fn run(self) {
         println!("rpc starts");
 
         tokio::time::sleep(Duration::from_secs(100)).await;
