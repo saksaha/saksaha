43344b1f95089b159239649936d32da2d534b073
Elden Park <elden@saksaha.com>
2022-08-10 18:56:10 +0900

    o

--- external/envelope_contract/src/envelope.rs
+++ external/envelope_contract/src/envelope.rs
@@ -160,102 +160,100 @@ fn handle_open_channel(
     storage: &mut Storage,
     args: RequestArgs,
 ) -> Result<InvokeResult, ContractError> {
-    return Ok(storage.to_vec());
+    let mut evl_storage: EnvelopeStorage = match serde_json::from_slice(storage)
+    {
+        Ok(s) => s,
+        Err(err) => {
+            return Err(format!(
+                "Could not parse storage into envelope_storage, err: {}",
+                err,
+            )
+            .into())
+        }
+    };
 
-    // let mut evl_storage: EnvelopeStorage = match serde_json::from_slice(storage)
-    // {
-    //     Ok(s) => s,
-    //     Err(err) => {
-    //         return Err(format!(
-    //             "Could not parse storage into envelope_storage, err: {}",
-    //             err,
-    //         )
-    //         .into())
-    //     }
-    // };
+    let open_ch_params: OpenChParams = match serde_json::from_slice(&args) {
+        Ok(p) => p,
+        Err(err) => {
+            return Err(
+                format!("Could not parse open ch params, err: {}", err).into()
+            )
+        }
+    };
 
-    // let open_ch_params: OpenChParams = match serde_json::from_slice(&args) {
-    //     Ok(p) => p,
-    //     Err(err) => {
-    //         return Err(
-    //             format!("Could not parse open ch params, err: {}", err).into()
-    //         )
-    //     }
-    // };
+    let dst_pk = open_ch_params.dst_pk;
+    let open_ch = open_ch_params.open_ch;
 
-    // let dst_pk = open_ch_params.dst_pk;
-    // let open_ch = open_ch_params.open_ch;
+    match evl_storage.chats.get_mut(&open_ch.ch_id) {
+        Some(_) => {
+            return Err(format!("The channel is already opened").into());
+        }
+        None => {}
+    };
 
-    // match evl_storage.chats.get_mut(&open_ch.ch_id) {
-    //     Some(_) => {
-    //         return Err(format!("The channel is already opened").into());
-    //     }
-    //     None => {}
-    // };
+    match evl_storage.open_ch_reqs.get_mut(&dst_pk) {
+        Some(open_channels) => {
+            // let mut open_ch_data: Vec<String> =
+            //     match serde_json::from_str(&o.as_str()) {
+            //         Ok(vs) => vs,
+            //         Err(err) => {
+            //             return Err(ContractError::new(
+            //                 format!("err: {:?}", err).into(),
+            //             ));
+            //         }
+            //     };
 
-    // match evl_storage.open_ch_reqs.get_mut(&dst_pk) {
-    //     Some(open_channels) => {
-    //         // let mut open_ch_data: Vec<String> =
-    //         //     match serde_json::from_str(&o.as_str()) {
-    //         //         Ok(vs) => vs,
-    //         //         Err(err) => {
-    //         //             return Err(ContractError::new(
-    //         //                 format!("err: {:?}", err).into(),
-    //         //             ));
-    //         //         }
-    //         //     };
+            // open_ch_data.push(input_serialized.clone());
 
-    //         // open_ch_data.push(input_serialized.clone());
+            // let input_serialized_new =
+            //     match serde_json::to_string(&open_ch_data) {
+            //         Ok(s) => s,
+            //         Err(err) => {
+            //             return Err(ContractError::new(
+            //                 format!("err: {:?}", err).into(),
+            //             ));
+            //         }
+            //     };
 
-    //         // let input_serialized_new =
-    //         //     match serde_json::to_string(&open_ch_data) {
-    //         //         Ok(s) => s,
-    //         //         Err(err) => {
-    //         //             return Err(ContractError::new(
-    //         //                 format!("err: {:?}", err).into(),
-    //         //             ));
-    //         //         }
-    //         //     };
+            open_channels.push(open_ch);
 
-    //         open_channels.push(open_ch);
+            // serde_json::to_vec
 
-    //         // serde_json::to_vec
+            // msg_storage.insert(dst_pk.clone(), open_channels);
+        }
+        None => {
+            // let mut open_ch_data = vec![];
 
-    //         // msg_storage.insert(dst_pk.clone(), open_channels);
-    //     }
-    //     None => {
-    //         // let mut open_ch_data = vec![];
+            // open_ch_data.push(input_serialized.clone());
 
-    //         // open_ch_data.push(input_serialized.clone());
+            // let input_serialized_new =
+            //     match serde_json::to_string(&open_ch_data) {
+            //         Ok(s) => s,
+            //         Err(err) => {
+            //             return Err(ContractError::new(
+            //                 format!("err: {:?}", err).into(),
+            //             ));
+            //         }
+            //     };
 
-    //         // let input_serialized_new =
-    //         //     match serde_json::to_string(&open_ch_data) {
-    //         //         Ok(s) => s,
-    //         //         Err(err) => {
-    //         //             return Err(ContractError::new(
-    //         //                 format!("err: {:?}", err).into(),
-    //         //             ));
-    //         //         }
-    //         //     };
+            evl_storage.open_ch_reqs.insert(dst_pk, vec![open_ch]);
 
-    //         evl_storage.open_ch_reqs.insert(dst_pk, vec![open_ch]);
+            // storage.insert(dst_pk.clone(), input_serialized_new.clone());
+        }
+    };
 
-    //         // storage.insert(dst_pk.clone(), input_serialized_new.clone());
-    //     }
-    // };
+    *storage = match serde_json::to_vec(&evl_storage) {
+        Ok(s) => s,
+        Err(err) => {
+            return Err(format!(
+                "Cannot serialize envelope storage, err: {}",
+                err
+            )
+            .into())
+        }
+    };
 
-    // *storage = match serde_json::to_vec(&evl_storage) {
-    //     Ok(s) => s,
-    //     Err(err) => {
-    //         return Err(format!(
-    //             "Cannot serialize envelope storage, err: {}",
-    //             err
-    //         )
-    //         .into())
-    //     }
-    // };
-
-    // Ok(vec![])
+    Ok(vec![])
 }
 
 fn handle_send_msg(
--- source/sak_contract_std/src/v0/storage.rs
+++ source/sak_contract_std/src/v0/storage.rs
@@ -1,14 +1,4 @@
 use crate::StorageError;
 use std::collections::HashMap;
 
-// pub type Storage = HashMap<String, String>;
 pub type Storage = Vec<u8>;
-
-// pub fn parse(data: &[u8]) -> Result<Storage, StorageError> {
-//     match serde_json::from_slice(data) {
-//         Ok(s) => Ok(s),
-//         Err(err) => {
-//             return Err(err.into());
-//         }
-//     }
-// }
