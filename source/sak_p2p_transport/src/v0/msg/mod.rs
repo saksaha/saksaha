a39b98fb94bb5a881853923e32a0bdd53a7567b3
Elden Park <elden@saksaha.com>
2022-08-10 11:05:46 +0900

    o

--- source/sak_p2p_transport/src/v0/msg/mod.rs
+++ source/sak_p2p_transport/src/v0/msg/mod.rs
@@ -5,8 +5,7 @@ mod msg;
 mod msg_type;
 mod ping;
 mod tx;
-mod tx_hash_syn;
-mod tx_syn;
+pub(crate) mod tx_utils;
 
 pub use block_hash_syn::*;
 pub use block_syn::*;
@@ -14,6 +13,4 @@ pub use handshake::Handshake;
 pub use msg::Msg;
 pub(crate) use msg_type::*;
 pub use ping::*;
-pub(crate) use tx::*;
-pub use tx_hash_syn::*;
-pub use tx_syn::*;
+pub use tx::*;
