a2038731c3746d6cfe306d8a58782cfcad710c43
Elden Park <eldeniyenden@gmail.com>
2022-08-12 03:21:18 +0900

    c

--- source/saksaha_network/src/node/peer_node.rs
+++ source/saksaha_network/src/node/peer_node.rs
@@ -29,6 +29,33 @@ impl PeerNode {
         let public_key = self.peer.get_public_key_short();
         let node_task_queue = self.node_task_queue.clone();
 
+        {
+            // Ledger event routine
+            let ledger_event_rx = {
+                let rx = self
+                    .machine
+                    .blockchain
+                    .dist_ledger
+                    .ledger_event_tx
+                    .clone()
+                    .read()
+                    .await
+                    .subscribe();
+
+                rx
+            };
+
+            let mut ledger_event_routine = LedgerEventRoutine {
+                ledger_event_rx,
+                machine: self.machine.clone(),
+                node_task_queue: node_task_queue.clone(),
+            };
+
+            tokio::spawn(async move {
+                ledger_event_routine.run().await;
+            });
+        }
+
         loop {
             let mut conn_lock = self.peer.get_transport().conn.write().await;
 
@@ -58,69 +85,6 @@ impl PeerNode {
                     return;
                 }
             };
-
-            // tokio::select! {
-            //     Ok(task) = self.node_task_queue.pop_front() => {
-            //         // let blocks = self.machine
-            //         //     .blockchain
-            //         //     .dist_ledger
-            //         //     .apis
-            //         //     .get_entire_block_info_list()
-            //         //     .await
-            //         //     .unwrap_or(vec![]);
-
-            //         // match conn
-            //         //     .socket
-            //         //     .send(Msg::BlockHashSyn(
-            //         //         BlockHashSynMsg {
-            //         //             new_blocks: blocks
-            //         //         }
-            //         //     ))
-            //         //     .await
-            //         // {
-            //         //     Ok(_) => {
-            //         //         debug!("Sending BlockHashSyn",);
-            //         //     }
-            //         //     Err(err) => {
-            //         //         warn!("Failed to BlockHashSyn, err: {}", err,);
-            //         //     }
-            //         // };
-            //     },
-            //     // maybe_msg =
-            //         conn_lock
-            //         .next_msg() => {
-            //         println!("2222222222, pub_key: {}",
-            //             self.peer.get_public_key_short());
-
-            //         match maybe_msg {
-            //             Some(maybe_msg) => match maybe_msg {
-            //                 Ok(msg) => {
-            //                     let _ = msg_handle::handle_msg(
-            //                         msg,
-            //                         &self.machine,
-            //                         &mut conn_lock,
-            //                         &self.node_task_queue,
-            //                         &self.peer,
-            //                     ).await;
-            //                 }
-            //                 Err(err) => {
-            //                     warn!("Failed to parse the msg, err: {}", err);
-            //                 }
-            //             }
-            //             None => {
-            //                 warn!("Peer has ended the connection");
-
-            //                 self.peer.set_peer_status(PeerStatus::Disconnected)
-            //                     .await;
-
-            //                 return;
-            //             }
-            //         };
-
-            //         println!("---end 2222222222, pub_key: {}",
-            //             self.peer.get_public_key_short());
-            //     }
-            // };
         }
     }
 
