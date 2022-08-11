673f5e046b7b8b37063088a8341035929e91fa66
Elden Park <eldeniyenden@gmail.com>
2022-08-11 11:41:44 +0900

    o

--- source/saksaha_network/src/node/local_node.rs
+++ source/saksaha_network/src/node/local_node.rs
@@ -2,7 +2,7 @@ use super::{
     event_handle::BlockchainEventRoutine,
     miner::Miner,
     peer_node::PeerNode,
-    task::{runtime, NodeRuntimeCtx, NodeTask},
+    task::{NodeTask, NodeTaskRuntime, NodeTaskRuntimeCtx},
 };
 use crate::machine::Machine;
 use futures::Future;
@@ -23,56 +23,62 @@ impl LocalNode {
     pub(crate) async fn run(&self) {
         let machine = self.machine.clone();
         let mine_interval = self.mine_interval.clone();
+        let node_task_queue = Arc::new(TaskQueue::new(100));
 
-        if self.miner {
-            tokio::spawn(async move {
-                let mut miner = Miner::init(machine, mine_interval);
+        {
+            // miner routine
+            if self.miner {
+                tokio::spawn(async move {
+                    let mut miner = Miner::init(machine, mine_interval);
 
-                miner.run().await;
-            });
+                    miner.run().await;
+                });
+            }
         }
 
-        let machine = self.machine.clone();
-        let bc_event_rx = {
-            let rx = machine
-                .blockchain
-                .dist_ledger
-                .bc_event_tx
-                .clone()
-                .read()
-                .await
-                .subscribe();
-
-            rx
-        };
-
-        let node_task_queue = Arc::new(TaskQueue::new(100));
-        let node_task_queue_clone = node_task_queue.clone();
-        let node_task_min_interval = self.node_task_min_interval.clone();
-        let node_runtime_ctx = NodeRuntimeCtx {
-            peer_table: self.peer_table.clone(),
-        };
-
-        tokio::spawn(async move {
-            let node_task_runtime = runtime::create_node_task_runtime(
+        {
+            // node task routine
+            let node_task_queue_clone = node_task_queue.clone();
+            let node_runtime_ctx = NodeTaskRuntimeCtx {
+                peer_table: self.peer_table.clone(),
+            };
+            let node_task_runtime = NodeTaskRuntime::new(
                 node_task_queue_clone,
-                node_task_min_interval,
+                self.node_task_min_interval,
                 node_runtime_ctx,
             );
 
-            // node_task_runtime.run().await;
-        });
+            tokio::spawn(async move {
+                node_task_runtime.run().await;
+            });
+        }
 
-        let mut bc_event_routine = BlockchainEventRoutine {
-            bc_event_rx,
-            // public_key: public_key.to_string(),
-            machine: self.machine.clone(),
-            node_task_queue: node_task_queue.clone(),
-        };
+        {
+            // blockchain event routine
+            let machine = self.machine.clone();
+            let bc_event_rx = {
+                let rx = machine
+                    .blockchain
+                    .dist_ledger
+                    .bc_event_tx
+                    .clone()
+                    .read()
+                    .await
+                    .subscribe();
 
-        tokio::spawn(async move {
-            bc_event_routine.run().await;
-        });
+                rx
+            };
+
+            let mut bc_event_routine = BlockchainEventRoutine {
+                bc_event_rx,
+                machine: self.machine.clone(),
+                node_task_queue: node_task_queue.clone(),
+            };
+
+            tokio::spawn(async move {
+                bc_event_routine.run().await;
+            });
+        }
 
         let peer_it = self.peer_table.new_iter();
         let mut peer_it_lock = peer_it.write().await;
