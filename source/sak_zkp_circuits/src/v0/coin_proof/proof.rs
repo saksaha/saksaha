91febaaa2228cbcb8c501b3ab38e761c7ec9382d
Elden Park <elden@saksaha.com>
2022-08-31 18:18:13 +0900

    o

--- source/sak_zkp_circuits/src/v0/coin_proof/proof.rs
+++ source/sak_zkp_circuits/src/v0/coin_proof/proof.rs
@@ -1,28 +1,12 @@
-use crate::ProofError;
-use bellman::groth16::{
-    self, Parameters, PreparedVerifyingKey, Proof, VerifyingKey,
+use crate::{
+    get_mimc_params_1_to_2, CoinProofCircuit1to2, Hasher, NewCoin, OldCoin,
+    ProofError,
 };
+use bellman::groth16::{self, PreparedVerifyingKey, Proof, VerifyingKey};
 use pairing::MultiMillerLoop;
 use sak_crypto::{Bls12, OsRng, Scalar};
-use sak_zkp_circuits::{CoinProofCircuit1to2, Hasher, NewCoin, OldCoin};
-
-const CIRCUIT_PARAMS_1TO2: &[u8] =
-    include_bytes!("../../../../prebuild/circuit_params_1to2");
 
 pub struct CoinProof;
-
-pub(crate) fn get_mimc_params_1_to_2(
-    constants: &[Scalar],
-) -> Result<Parameters<Bls12>, ProofError> {
-    match Parameters::<Bls12>::read(&CIRCUIT_PARAMS_1TO2[..], false) {
-        Ok(p) => Ok(p),
-        Err(err) => {
-            return Err(
-                format!("Error getting circuit params, err: {}", err).into()
-            );
-        }
-    }
-}
 
 impl CoinProof {
     pub fn make_verifying_key<E: MultiMillerLoop>(
