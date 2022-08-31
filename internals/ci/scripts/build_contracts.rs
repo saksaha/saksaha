260d04502c056c79a3e2e26a212a4692ddcebb98
Elden Park <eldeniyenden@gmail.com>
2022-08-31 18:38:41 +0900

    ci: do not clean prebuild before "build_contracts"

--- internals/ci/scripts/build_contracts.rs
+++ internals/ci/scripts/build_contracts.rs
@@ -3,7 +3,7 @@ use crate::CIError;
 use std::env::Args;
 
 pub(crate) fn run(_args: Args) -> Result<(), CIError> {
-    tasks::clean_prebuild()?;
+    // tasks::clean_prebuild()?;
 
     tasks::build_system_contracts()?;
     tasks::build_3rd_party_contracts()?;
