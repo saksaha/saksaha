9acfb2002c41861a1a207724c1a018abe90a11e8
Elden Park <elden@saksaha.com>
2022-09-01 18:19:06 +0900

    o

--- source/sak_proofs/src/v0/coin_proof/mod.rs
+++ source/sak_proofs/src/v0/coin_proof/mod.rs
@@ -3,7 +3,3 @@ mod proof;
 
 pub use coin::*;
 pub use proof::*;
-
-// pub const CM_TREE_DEPTH: u32 = 4;
-
-// pub const CM_TREE_CAPACITY: usize = 2_usize.pow(CM_TREE_DEPTH as u32);
--- source/sak_proofs/src/v0/mod.rs
+++ source/sak_proofs/src/v0/mod.rs
@@ -1,14 +1,7 @@
 mod coin_proof;
-mod hasher;
-mod merkle;
-mod mimc;
 
 #[cfg(test)]
 mod tests;
-
-pub use coin_proof::*;
-pub use hasher::*;
-pub use merkle::*;
 
 pub use bellman::gadgets::boolean::AllocatedBit;
 pub use bellman::groth16::{
@@ -19,6 +12,7 @@ pub use bellman::{groth16, Circuit, ConstraintSystem, SynthesisError};
 pub use bls12_381::{Bls12, Scalar};
 
 // Saksaha zkp circuits
+pub use coin_proof::*;
 pub use sak_zkp_circuits::{
     get_parent_idx, Hasher, MerkleTree, NewCoin, OldCoin, Path, CM_TREE_DEPTH,
 };
