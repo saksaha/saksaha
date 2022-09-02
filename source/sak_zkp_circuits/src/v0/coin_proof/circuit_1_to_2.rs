2481f612dda274c9244f94cf60fc3bb552a25bfb
Elden Park <elden@saksaha.com>
2022-09-02 11:39:40 +0900

    zkp_circuit+: make CM_TREE_DEPTH managed in a single place

--- source/sak_zkp_circuits/src/v0/coin_proof/circuit_1_to_2.rs
+++ source/sak_zkp_circuits/src/v0/coin_proof/circuit_1_to_2.rs
@@ -1,20 +1,10 @@
-use crate::{
-    CircuitError, Hasher, NewCoin, OldCoin, ProofError, CM_TREE_DEPTH,
-};
+use crate::{CircuitError, Hasher, NewCoin, OldCoin, CM_TREE_DEPTH};
 use bellman::gadgets::boolean::AllocatedBit;
-use bellman::groth16::{self, Parameters};
 use bellman::{Circuit, ConstraintSystem, SynthesisError};
-use sak_crypto::{Bls12, OsRng, Scalar};
-use std::fs::File;
-use std::io::Write;
+use sak_crypto::Scalar;
 use type_extension::U8Array;
 
 pub const GAS: u64 = 10;
-
-// const PARAM_FILE_NAME: &str = "mimc_params_1_to_2";
-
-// const CIRCUIT_PARAMS_1TO2: &[u8] =
-//     include_bytes!("../../../../../prebuild/circuit_params_1to2");
 
 pub struct CoinProofCircuit1to2 {
     pub hasher: Hasher,
@@ -26,44 +16,6 @@ pub struct CoinProofCircuit1to2 {
     pub coin_2_new: NewCoin,
 
     pub constants: Vec<Scalar>,
-}
-
-pub(crate) fn generate_circuit_params(
-    constants: &[Scalar],
-) -> Result<Parameters<Bls12>, CircuitError> {
-    let hasher = Hasher::new();
-
-    let coin_1_old = OldCoin::default();
-    let coin_1_new = NewCoin::default();
-    let coin_2_new = NewCoin::default();
-
-    let params = {
-        let c = CoinProofCircuit1to2 {
-            hasher,
-            coin_1_old,
-            coin_1_new,
-            coin_2_new,
-            constants: constants.to_vec(),
-        };
-
-        groth16::generate_random_parameters::<Bls12, _, _>(c, &mut OsRng)?
-    };
-
-    Ok(params)
-}
-
-pub(crate) fn get_mimc_params_1_to_2(
-    // constants: &[Scalar],
-    circuit_params: &[u8],
-) -> Result<Parameters<Bls12>, ProofError> {
-    match Parameters::<Bls12>::read(circuit_params, false) {
-        Ok(p) => Ok(p),
-        Err(err) => {
-            return Err(
-                format!("Error getting circuit params, err: {}", err).into()
-            );
-        }
-    }
 }
 
 impl Circuit<Scalar> for CoinProofCircuit1to2 {
