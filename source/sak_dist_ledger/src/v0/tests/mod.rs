a226f566a95cd1ea1d82d47aea958ab48f80a032
Noah <noah@saksaha.com>
2022-09-05 19:32:08 +0900

    dist_ledger: modify "CM_TREE_DEPTH" to be (4->) 6
    Co-authored-by: Elden Park <elden@saksaha.com>

--- source/sak_dist_ledger/src/v0/tests/mod.rs
+++ source/sak_dist_ledger/src/v0/tests/mod.rs
@@ -2,4 +2,3 @@ mod block;
 mod others;
 mod test_util;
 mod tx;
-mod utils;
--- source/sak_dist_ledger_meta/src/v0/mod.rs
+++ source/sak_dist_ledger_meta/src/v0/mod.rs
@@ -1,4 +1,4 @@
-pub const CM_TREE_DEPTH: u32 = 4;
+pub const CM_TREE_DEPTH: u32 = 6;
 
 pub const CM_TREE_CAPACITY: usize = 2_usize.pow(CM_TREE_DEPTH as u32);
 
