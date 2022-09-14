8b4d090bcde32b417cb9ab5a6c4082ffbbbadb93
c0np4nn4 <aaron@saksaha.com>
2022-09-14 16:48:23 +0900

    tmux: refactor 2clis launch script (use variables and compartmentalize)

--- internals/tmux/launch_dev_2_clis.sh
+++ internals/tmux/launch_dev_2_clis.sh
@@ -1,19 +1,29 @@
 #!/bin/bash
-tmux new-session -d -s saksaha
-tmux split-window -h
-tmux split-window -v
-tmux split-window -v
 
-tmux select-pane -t 1
-tmux split-window -v
-tmux split-window -v
+# initialize
+NETWORK_CONFIG="$HOME/.config/saksaha"
+if [ -d $NETWORK_CONFIG ]; then
+  rm -rf $NETWORK_CONFIG
+fi
 
-tmux select-layout tiled
+WALLET_CONFIG="$HOME/.config/saksaha-wallet"
+if [ -d $WALLET_CONFIG ]; then
+  rm -rf $WALLET_CONFIG
+fi
 
-tmux resize-pane -t 1 -U 16
-tmux resize-pane -t 3 -U 16
-# 6 panes have been displayed
+# profiles
+PROFILE_1="--cfg-profile dev_local_1"
+PROFILE_2="--cfg-profile dev_local_2"
 
+# ci path 
+SAKSAHA_WORKSPACE=$(dirname $(find $HOME -path \*/saksaha/internals 2>/dev/null))
+CI="$SAKSAHA_WORKSPACE/ci"
+
+# endpoint to node 2
+ENDPOINT_TO_NODE_2="--saksaha-endpoint http://localhost:34419/rpc/v0"
+
+
+prepare_saksaha_session() {
 # |---------------|---------------|
 # | 1: node_1     | 2: node_2     |
 # |---------------|---------------|
@@ -21,62 +31,63 @@ tmux resize-pane -t 3 -U 16
 # |---------------|---------------|
 # | 5: evl_term_1 | 6: evl_term_2 |
 # |---------------|---------------|
+  tmux new-session -d -s saksaha
+  tmux split-window -h
+  tmux split-window -v
+  tmux split-window -v
+
+  tmux select-pane -t 1
+  tmux split-window -v
+  tmux split-window -v
+
+  tmux select-layout tiled
+
+  tmux resize-pane -t 5 -U 32
+}
+
+run_nodes() {
+  tmux select-pane -t 1
+  tmux send-keys "${CI} dev ${PROFILE_1}" ENTER
+
+  sleep 0.5
+
+  tmux select-pane -t 2
+  tmux send-keys "${CI} dev ${PROFILE_2}" ENTER
+}
+
+run_wallets() {
+  sleep 0.1
+
+  tmux select-pane -t 3
+  tmux send-keys "${CI} dev_wallet ${PROFILE_1}" ENTER
+
+  sleep 0.1
+
+  tmux select-pane -t 4
+  tmux send-keys "${CI} dev_wallet ${PROFILE_2} ${ENDPOINT_TO_NODE_2}" ENTER
+}
+
+run_evl_terms() {
+  sleep 0.1
+
+  tmux select-pane -t 5
+  tmux send-keys "${CI} dev_evl_term ${PROFILE_1}" ENTER
+
+  sleep 0.1
+
+  tmux select-pane -t 6
+  tmux send-keys "${CI} dev_evl_term ${PROFILE_2} ${ENDPOINT_TO_NODE_2}" ENTER
+}
+
+attach_to_saksaha_session() {
+  tmux attach-session -t saksaha
+}
 
 
-ci="~/work/saksaha/saksaha/ci"
+prepare_saksaha_session
 
-clear_node_1="rm -rf ~/.config/saksaha/dev_local_1/"
-clear_node_2="rm -rf ~/.config/saksaha/dev_local_2/"
+run_nodes
+run_wallets
+run_evl_terms
 
-profile_1="--cfg-profile dev_local_1"
-profile_2="--cfg-profile dev_local_2"
-
-run_node_1="${ci} dev ${profile_1}"
-run_node_2="${ci} dev ${profile_2}"
-
-clear_wallet="rm -rf ~/.config/saksaha-wallet"
-
-run_wallet_1="${ci} dev_wallet ${profile_1}"
-run_wallet_2="${ci} dev_wallet ${profile_2}"
-
-endpoint_to_node_2="--saksaha-endpoint http://localhost:34419/rpc/v0"
-
-run_evl_term_1="${ci} dev_evl_term ${profile_1}"
-run_evl_term_2="${ci} dev_evl_term ${profile_2}"
-
-
-# node
-tmux select-pane -t 1
-tmux send-keys "${clear_node_1}" ENTER
-tmux send-keys "${run_node_1}" ENTER
-
-sleep 0.5
-
-tmux select-pane -t 2
-tmux send-keys "${clear_node_2}" ENTER
-tmux send-keys "${run_node_2}" ENTER
-
-sleep 0.1
-
-# wallet
-tmux select-pane -t 3
-tmux send-keys "${clear_wallet}" ENTER
-tmux send-keys "${run_wallet_1}" ENTER
-
-sleep 0.1
-
-tmux select-pane -t 4
-tmux send-keys "${run_wallet_2} ${endpoint_to_node_2}" ENTER
-
-sleep 0.1
-
-# term
-tmux select-pane -t 5
-tmux send-keys "${run_evl_term_1}" ENTER
-
-sleep 0.1
-
-tmux select-pane -t 6
-tmux send-keys "${run_evl_term_2} ${endpoint_to_node_2}" ENTER
-
-tmux attach-session -t saksaha
+attach_to_saksaha_session
