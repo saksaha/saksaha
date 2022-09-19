94f599fc68cd643c961bc219dee0f2271622d424
Elden Park <elden@saksaha.com>
2022-09-19 16:19:57 +0900

    o

--- source/sak_fs/src/v0/paths.rs
+++ source/sak_fs/src/v0/paths.rs
@@ -1,7 +1,5 @@
 use crate::FSError;
-use colored::Colorize;
 use directories::ProjectDirs;
-use log::info;
 use std::fs;
 use std::path::PathBuf;
 
@@ -21,21 +19,3 @@ pub fn get_config_dir(app_name: &str) -> Result<PathBuf, FSError> {
         .into());
     }
 }
-
-// // {home}/{config}/{app_name}/{app_prefix}/...
-// pub fn create_or_get_app_path(app_name: &str) -> Result<PathBuf, FSError> {
-//     if let Some(dir) = ProjectDirs::from("com", "Saksaha", app_name) {
-//         let app_root_path = dir.config_dir();
-
-//         if !app_root_path.exists() {
-//             fs::create_dir_all(app_root_path)?;
-//         }
-
-//         return Ok(app_root_path.to_path_buf());
-//     } else {
-//         return Err(format!(
-//             "No valid app (config) path provided by the operating system"
-//         )
-//         .into());
-//     }
-// }
