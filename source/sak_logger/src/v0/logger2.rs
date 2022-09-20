2ef434e91cff39a340d696b902228d5b6c3ed4fb
Elden Park <elden@saksaha.com>
2022-09-20 17:51:52 +0900

    o

--- source/sak_logger/src/v0/logger2.rs
+++ source/sak_logger/src/v0/logger2.rs
@@ -1,5 +1,6 @@
 use crate::{LoggerError, RUST_LOG_ENV};
 use chrono::Local;
+use colored::Colorize;
 use env_logger::{Builder, Env, Logger};
 use std::cmp::min;
 use std::fs::File;
@@ -7,7 +8,7 @@ use std::io;
 use std::io::Write;
 use std::path::PathBuf;
 use std::sync::atomic::AtomicBool;
-use tracing::{Event, Subscriber};
+use tracing::{Event, Level, Subscriber};
 use tracing_subscriber;
 use tracing_subscriber::fmt::{
     format, FmtContext, FormatEvent, FormatFields, FormattedFields,
@@ -33,8 +34,12 @@ pub fn setup_logger2(log_dir: &PathBuf) -> Result<(), LoggerError> {
 
     let file = std::fs::File::create(&log_file_path).unwrap();
 
+    let file_appender =
+        tracing_appender::rolling::hourly(log_dir, "saksaha.log");
+    let (non_blocking, _guard) = tracing_appender::non_blocking(file_appender);
+
     let layer = tracing_subscriber::fmt::layer()
-        .event_format(MyFormatter)
+        .event_format(ConsoleLogFormatter)
         .with_filter(EnvFilter::from_default_env())
         .boxed();
 
@@ -43,7 +48,7 @@ pub fn setup_logger2(log_dir: &PathBuf) -> Result<(), LoggerError> {
     let layer = tracing_subscriber::fmt::layer()
         .with_thread_names(true)
         .with_target(true)
-        .event_format(MyFormatter)
+        .event_format(FileLogFormatter)
         .with_writer(file)
         .with_filter(EnvFilter::from_default_env())
         .boxed();
@@ -59,9 +64,9 @@ pub fn setup_logger2(log_dir: &PathBuf) -> Result<(), LoggerError> {
     Ok(())
 }
 
-struct MyFormatter;
+struct ConsoleLogFormatter;
 
-impl<S, N> FormatEvent<S, N> for MyFormatter
+impl<S, N> FormatEvent<S, N> for ConsoleLogFormatter
 where
     S: Subscriber + for<'a> LookupSpan<'a>,
     N: for<'a> FormatFields<'a> + 'static,
@@ -77,13 +82,23 @@ where
 
         let now = Local::now().format("%y-%m-%d %H:%M:%S");
 
-        write!(
-            &mut writer,
-            "{} {:>5} {}: ",
-            now,
-            metadata.level(),
-            metadata.target()
-        )?;
+        let level = match metadata.level().as_str() {
+            "INFO" => "INFO".green(),
+            "WARN" => "WARN".yellow(),
+            "ERROR" => "ERROR".red(),
+            "DEBUG" => "DEBUG".blue(),
+            _ => "".green(),
+        };
+
+        let target = metadata.target();
+        let target_len = target.len();
+        let target = if target_len > 16 {
+            &target[target_len - 16..target_len]
+        } else {
+            &target
+        };
+
+        write!(&mut writer, "{} {:>5} {:>16}: ", now, level, target,)?;
 
         // Format all the spans in the event's span context.
         if let Some(scope) = ctx.event_scope() {
@@ -101,6 +116,54 @@ where
                     .expect("will never be `None`");
 
                 // Skip formatting the fields if the span had no fields.
+                if !fields.is_empty() {
+                    write!(writer, "{{{}}}", fields)?;
+                }
+                write!(writer, ": ")?;
+            }
+        }
+
+        // Write fields on the event
+        ctx.field_format().format_fields(writer.by_ref(), event)?;
+
+        writeln!(writer)
+    }
+}
+
+struct FileLogFormatter;
+
+impl<S, N> FormatEvent<S, N> for FileLogFormatter
+where
+    S: Subscriber + for<'a> LookupSpan<'a>,
+    N: for<'a> FormatFields<'a> + 'static,
+{
+    fn format_event(
+        &self,
+        ctx: &FmtContext<'_, S, N>,
+        mut writer: format::Writer<'_>,
+        event: &Event<'_>,
+    ) -> std::fmt::Result {
+        let metadata = event.metadata();
+
+        let now = Local::now().format("%y-%m-%d %H:%M:%S");
+
+        write!(
+            &mut writer,
+            "{} {:>5} {}: ",
+            now,
+            metadata.level(),
+            metadata.target()
+        )?;
+
+        if let Some(scope) = ctx.event_scope() {
+            for span in scope.from_root() {
+                write!(writer, "{}", span.name())?;
+
+                let ext = span.extensions();
+                let fields = &ext
+                    .get::<FormattedFields<N>>()
+                    .expect("will never be `None`");
+
                 if !fields.is_empty() {
                     write!(writer, "{{{}}}", fields)?;
                 }
