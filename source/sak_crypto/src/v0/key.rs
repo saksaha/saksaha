5dd4b755e2a88bf5b8ad3dc14cb99b27ac470ed0
Noah <noah@saksaha.com>
2022-09-20 18:45:06 +0900

    jni+: add a few "crypto" apis including creating credential

--- source/sak_crypto/src/v0/key.rs
+++ source/sak_crypto/src/v0/key.rs
@@ -1,8 +1,16 @@
-use crate::OsRng;
-use crate::PublicKey;
+use crate::{encode_hex, PublicKey};
+use crate::{CryptoError, OsRng};
 use k256::{elliptic_curve::sec1::ToEncodedPoint, SecretKey};
+use serde::{Deserialize, Serialize};
 
 pub struct SakKey;
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Credential {
+    pub public_key: String,
+    pub secret: String,
+    pub acc_addr: String,
+}
 
 impl SakKey {
     pub fn generate() -> (SecretKey, PublicKey) {
@@ -29,5 +37,23 @@ impl SakKey {
 
     pub fn foo() -> String {
         String::from("power 1")
+    }
+}
+
+impl Credential {
+    pub fn new_random() -> Result<Credential, CryptoError> {
+        let (sk, pk) = SakKey::generate();
+        let secret = encode_hex(&sk.to_bytes());
+        let public_key = encode_hex(&pk.to_encoded_point(false).to_bytes());
+
+        let acc_addr = SakKey::create_acc_addr(&pk);
+
+        let c = Credential {
+            public_key,
+            secret,
+            acc_addr,
+        };
+
+        Ok(c)
     }
 }
