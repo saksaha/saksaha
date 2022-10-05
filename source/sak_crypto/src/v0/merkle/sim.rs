ac223f470d917b8b2e648d4e57f848343db8df48
Kihyun Kim <kihyun@saksaha.com>
2022-10-05 17:34:42 +0900

    o

--- source/sak_crypto/src/v0/merkle/sim.rs
+++ source/sak_crypto/src/v0/merkle/sim.rs
@@ -1,8 +1,10 @@
+use crate::hasher::MiMC;
 use crate::CryptoError;
 use crate::MerkleTree;
 use crate::Scalar;
 use crate::ScalarExt;
 use std::collections::HashMap;
+use std::convert::TryInto;
 use type_extension::U8Array;
 
 // {height}_{idx}. Index starts from 0.
@@ -19,72 +21,85 @@ pub struct MerkleTreeSim {
 
 impl MerkleTreeSim {
     pub fn init(height: u32, leaves: Vec<Scalar>) -> MerkleTreeSim {
-        MerkleTreeSim {
+        let mut mk_tree_init = MerkleTreeSim {
             height,
             nodes: HashMap::new(),
             leaf_count: 0,
             merkle_tree: MerkleTree::new(height),
+        };
+        for (leaf_idx, leaf) in leaves.iter().enumerate() {
+            mk_tree_init.update_root(leaf_idx.try_into().unwrap(), *leaf);
         }
+
+        mk_tree_init
     }
 
     pub fn get_leaf_count(&self) -> u32 {
         self.leaf_count
     }
 
-    pub fn add_leaf_node(&mut self, cm: Scalar) {
-        self.leaf_count += 1;
-
-        let idx = format!("{}_{}", 0, self.leaf_count);
-
-        self.nodes.insert(idx, cm);
-
-        self.update_root(self.leaf_count);
+    pub fn get_merkle_rt(&self) -> Scalar {
+        *self
+            .nodes
+            .get(format!("{}_0", self.height).as_str())
+            .unwrap()
     }
 
-    pub fn update_root(&mut self, leaf_idx: u32) {
+    pub fn add_leaf_node(&mut self, cm: Scalar) {
+        let idx = format!("{}_{}", 0, self.leaf_count);
+
+        self.leaf_count += 1;
+
+        self.nodes.insert(idx, cm);
+    }
+
+    pub fn update_root(&mut self, leaf_idx: u32, leaf: Scalar) {
+        let hahser = MiMC::new();
         let auth_path = self.merkle_tree.generate_auth_paths(leaf_idx as u128);
 
-        println!("auth_path: {:?}", auth_path);
-
-        // for (h, path) in merkle_paths.iter().enumerate() {
-        //     println!("h: {}, path: {:?}", h, path);
-
-        //     let node = self.nodes.get(&path.idx_label).unwrap_or(
-        //         &ScalarExt::parse_arr(&U8Array::new_empty_32()).unwrap(),
-        //     );
-
-        //     if path.direction {
-
-        //     } else {
-        //     }
-        // }
+        self.add_leaf_node(leaf);
 
         for (height, path) in auth_path.iter().enumerate() {
             // let curr_idx = path.idx;
-            // let sibling_idx = match path.direction {
-            //     true => path.idx + 1,
-            //     false => path.idx - 1,
-            // };
+            let sibling_idx = path.idx;
 
-            // let sibling_loc = format!("{}_{}", height, sibling_idx);
-            // let sibling_node = match merkle_update.get(&sibling_loc) {
-            //     Some(n) => *n,
-            //     None => apis.get_merkle_node(&sibling_loc).await?,
-            // };
+            let sibling_loc;
+            let sibling_node: Scalar;
 
-            // let curr_loc = format!("{}_{}", height, curr_idx);
-            // let curr_node = match merkle_update.get(&curr_loc) {
-            //     Some(n) => *n,
-            //     None => apis.get_merkle_node(&curr_loc).await?,
-            // };
+            let curr_idx = match path.direction {
+                true => {
+                    let ci = sibling_idx + 1;
+                    sibling_loc = format!("{}_{}", height, sibling_idx);
+                    sibling_node = *self.nodes.get(&sibling_loc).unwrap();
+                    ci
+                }
+                false => {
+                    let ci = sibling_idx - 1;
+                    sibling_node = ScalarExt::parse_arr(&U8Array::new_empty_32()).unwrap();
+                    ci
+                }
+            };
 
-            // let merkle_node =
-            //     apis.hasher.mimc(&curr_node, &sibling_node)?.to_bytes();
+            let curr_loc = format!("{}_{}", height, curr_idx);
+            let curr_node = *self.nodes.get(&curr_loc).unwrap();
 
-            // let parent_idx = MerkleTree::get_parent_idx(curr_idx);
-            // let update_loc = format!("{}_{}", height + 1, parent_idx);
+            let lv;
+            let rv;
 
-            // merkle_update.insert(update_loc, merkle_node);
+            if path.direction {
+                lv = sibling_node;
+                rv = curr_node;
+            } else {
+                lv = curr_node;
+                rv = sibling_node;
+            }
+
+            let merkle_node = hahser.mimc_scalar(lv, rv);
+
+            let parent_idx = MerkleTree::get_parent_idx(curr_idx);
+            let update_loc = format!("{}_{}", height + 1, parent_idx);
+
+            self.nodes.insert(update_loc, merkle_node);
         }
     }
 }
