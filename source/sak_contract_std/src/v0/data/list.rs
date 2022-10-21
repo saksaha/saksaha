3d61b333c43600f3ca8e91ee74c6d1c9a43cbb4a
kihyun <kihyun@saksaha.com>
2022-10-21 19:01:27 +0900

    contract_std: implement LIST type draft
    * contract_std: implement field type `Dict` with function `get`
    
    * contract_std: seperate macro fns
    
    * move `alloc()` and `dealloc()` from `ctr_fns` to `ctr_default_fns`
    
    Co-authored-by: Aaron Cho <aaron@saksaha.com>

--- source/sak_contract_std/src/v0/data/list.rs
+++ source/sak_contract_std/src/v0/data/list.rs
@@ -18,6 +18,22 @@ impl List {
     pub fn receipt(&self) {}
 
     pub fn get(&self, key: &String) -> Vec<u8> {
+        let key: String = format!("{}_{}", self._name, key);
+
+        let data = get_mrs_data_from_host(&key);
+
+        data
+    }
+
+    pub fn push(&self, value: &String) -> Vec<u8> {
+        //TO-DO: get latest idx of the stored List
+
+        let latest_idx = 10;
+
+        let key: String = format!("{}_{}", self._name, latest_idx);
+
+        // push_mrs_data_to_host(&key, value);
+
         vec![0]
     }
 }
