1b8bc298498d6ac1c41df3bca1125d4ab5ae438e
Aaron Cho <aaron@saksaha.com>
2022-10-21 15:25:56 +0900

    contract_std: seperate macro fns
    * move `alloc()` and `dealloc()` from `ctr_fns` to `ctr_default_fns`

--- source/sak_contract_std/src/v0/ctr_utils.rs
+++ source/sak_contract_std/src/v0/ctr_utils.rs
@@ -1,16 +1,17 @@
 use std::convert::TryInto;
 
-use crate::RET_LEN_SIZE;
+use crate::{symbols::CTR__ALLOC, RET_LEN_SIZE};
 
 crate::define_host_ffi!();
+crate::define_ctr_default_fns!();
 
 pub fn get_mrs_data_from_host(key: &String) -> Vec<u8> {
     unsafe {
         let key_len = key.len();
-        let key_ptr = ctr_alloc(key_len);
+        let key_ptr = CTR__alloc(key_len);
         key_ptr.copy_from(key.as_ptr(), key_len);
 
-        let ret_len_ptr = ctr_alloc(RET_LEN_SIZE);
+        let ret_len_ptr = CTR__alloc(RET_LEN_SIZE);
         let ret_ptr = HOST__get_mrs_data(key_ptr, key_len as u32, ret_len_ptr as *mut u32);
         let ret_len = {
             let bytes: [u8; RET_LEN_SIZE] =
@@ -28,18 +29,4 @@ pub fn get_mrs_data_from_host(key: &String) -> Vec<u8> {
 
         data
     }
-}
-
-fn ctr_alloc(len: usize) -> *mut u8 {
-    let mut buf = Vec::with_capacity(len);
-    // take a mutable pointer to the buffer
-    let ptr = buf.as_mut_ptr();
-    // take ownership of the memory block and
-    // ensure the its destructor is not
-    // called when the object goes out of scope
-    // at the end of the function
-    std::mem::forget(buf);
-    // return the pointer so the runtime
-    // can write data at this offset
-    return ptr;
 }
--- source/sak_contract_std/src/v0/data/list.rs
+++ source/sak_contract_std/src/v0/data/list.rs
@@ -18,46 +18,6 @@ impl List {
     pub fn receipt(&self) {}
 
     pub fn get(&self, key: &String) -> Vec<u8> {
-        let data = unsafe {
-            // get_mrs_data_from_host(key);
-
-            let key_len = key.len();
-            let key_ptr = ctr_alloc(key_len);
-            key_ptr.copy_from(key.as_ptr(), key_len);
-
-            let ret_len_ptr = ctr_alloc(RET_LEN_SIZE);
-            let ret_ptr = HOST__get_mrs_data(key_ptr, key_len as u32, ret_len_ptr as *mut u32);
-            let ret_len = {
-                let bytes: [u8; RET_LEN_SIZE] =
-                    std::slice::from_raw_parts(ret_len_ptr as *mut u8, RET_LEN_SIZE)
-                        .try_into()
-                        .unwrap();
-                u32::from_be_bytes(bytes)
-            };
-
-            HOST__log(ret_len as i32, 135);
-
-            let data = Vec::from_raw_parts(ret_ptr as *mut u8, ret_len as usize, ret_len as usize);
-
-            HOST__log(3, 4);
-
-            data
-        };
-
-        data
+        vec![0]
     }
-}
-
-fn ctr_alloc(len: usize) -> *mut u8 {
-    let mut buf = Vec::with_capacity(len);
-    // take a mutable pointer to the buffer
-    let ptr = buf.as_mut_ptr();
-    // take ownership of the memory block and
-    // ensure the its destructor is not
-    // called when the object goes out of scope
-    // at the end of the function
-    std::mem::forget(buf);
-    // return the pointer so the runtime
-    // can write data at this offset
-    return ptr;
 }
