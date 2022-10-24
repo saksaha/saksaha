a057df6e7408862ace1accf9e875674720b541e2
Kihyun Kim <kihyun@saksaha.com>
2022-10-24 16:23:37 +0900

    o

--- source/sak_contract_std/src/v0/data/list.rs
+++ source/sak_contract_std/src/v0/data/list.rs
@@ -1,10 +1,10 @@
-use crate::{get_mrs_data_from_host, RET_LEN_SIZE};
+use crate::{get_mrs_data_from_host, ContractError, RET_LEN_SIZE};
 use std::{collections::HashMap, convert::TryInto};
 
 #[derive(Debug)]
 pub struct List {
     _name: String,
-    data: HashMap<String, String>,
+    data: HashMap<String, Vec<u8>>,
 }
 
 impl List {
@@ -23,17 +23,16 @@ impl List {
         data
     }
 
-    pub fn push(&self, value: &String) -> Vec<u8> {
+    pub fn push(&mut self, key: String, value: Vec<u8>) {
         //TO-DO: get latest idx of the stored List
+        let latest_idx = 0;
 
-        let latest_idx = 10;
-
-        let key: String = format!("{}_{}", self._name, latest_idx);
-
-        // push_mrs_data_to_host(&key, value);
-
-        vec![0]
+        self.data.insert(key, value);
     }
 
-    pub fn receipt(&self) {}
+    pub fn receipt(&self) -> Result<Vec<u8>, ContractError> {
+        let ret = serde_json::to_vec(&self.data)?;
+
+        Ok(ret)
+    }
 }
