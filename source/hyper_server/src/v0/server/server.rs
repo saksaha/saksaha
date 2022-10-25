975288ab243f98a448c0385e06a9043fe29c558c
Elden Park <elden@saksaha.com>
2022-10-25 18:50:45 +0900

    c

--- source/hyper_server/src/v0/server/server.rs
+++ source/hyper_server/src/v0/server/server.rs
@@ -1,25 +1,29 @@
 use super::{Middleware, StateMachine};
 use crate::RPCServerError;
+use futures::Future;
 use hyper::{
     server::conn::AddrIncoming,
     service::{self, make_service_fn, service_fn},
-    Body, Response, Server,
+    Body, Method, Request, Response, Server, StatusCode,
 };
 use sak_logger::{debug, error};
 use std::{convert::Infallible, sync::Arc};
 use tokio::net::TcpListener;
 
-pub struct RPCServer {}
+pub struct TCPServer {}
 
-impl RPCServer {
-    pub async fn run<C>(
+impl TCPServer {
+    pub async fn run<C, F, R, S>(
         self,
         tcp_socket: TcpListener,
         ctx: C,
         middlewares: Vec<Middleware<C>>,
+        f: F,
     ) -> Result<(), RPCServerError>
     where
         C: Clone + Send + Sync + 'static,
+        F: FnMut(Request<R>) -> S + Send,
+        S: Future + Send,
     {
         let addr_incoming = match AddrIncoming::from_listener(tcp_socket) {
             Ok(a) => a,
@@ -71,11 +75,29 @@ impl RPCServer {
         let server = Server::builder(addr_incoming);
 
         server.serve(make_service_fn(|_| async {
-            Ok::<_, hyper::Error>(service_fn(|_| async {
-                Ok::<_, hyper::Error>(Response::new(Body::from("")))
-            }))
+            Ok::<_, hyper::Error>(service_fn(
+                f, // |req| async move {
+                  // match (req.method(), req.uri().path()) {
+                  //     (&Method::POST, "/echo") => {
+                  //         let mut response = Response::new(Body::empty());
+                  //         *response.body_mut() = Body::from("Try POSTing data to /echo");
+                  //         Ok(response)
+                  //     }
+                  //     _ => {
+                  //         let mut not_found = Response::new(Body::empty());
+                  //         *not_found.status_mut() = StatusCode::NOT_FOUND;
+                  //         Ok::<_, hyper::Error>(not_found)
+                  //     }
+                  // }
+            ))
         }));
 
         Ok(())
     }
+}
+
+async fn a(req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
+    let mut not_found = Response::new(Body::empty());
+    *not_found.status_mut() = StatusCode::NOT_FOUND;
+    Ok::<_, hyper::Error>(not_found)
 }
