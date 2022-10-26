03881e15b738368f3111f03f9bd12e2fde06a0ca
Elden Park <elden@saksaha.com>
2022-10-26 22:23:13 +0900

    o

--- internals/ci/tasks/mod.rs
+++ internals/ci/tasks/mod.rs
@@ -2,8 +2,10 @@ mod circuit;
 mod clean;
 mod contracts;
 mod wasm_pack;
+mod yarn;
 
 pub(crate) use circuit::*;
 pub(crate) use clean::*;
 pub(crate) use contracts::*;
 pub(crate) use wasm_pack::*;
+pub(crate) use yarn::*;
---
+++ internals/ci/utils/exist.rs
@@ -0,0 +1,21 @@
+use crate::{log, CIError};
+use std::{
+    env,
+    path::{Path, PathBuf},
+};
+
+pub(crate) fn find_command<P>(exe_name: P) -> Option<PathBuf>
+where
+    P: AsRef<Path>,
+{
+    env::var_os("PATH").and_then(|paths| {
+        env::split_paths(&paths).find_map(|dir| {
+            let full_path = dir.join(&exe_name);
+            if full_path.is_file() {
+                Some(full_path)
+            } else {
+                None
+            }
+        })
+    })
+}
