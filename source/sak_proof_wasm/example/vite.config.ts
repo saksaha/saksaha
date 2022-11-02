5e42c1869aa19fe0f05091044b0a05d35a012381
Elden Park <eldeniyenden@gmail.com>
2022-11-02 14:12:13 +0900

    proof_wasm+: enable multi-threading in wasm
    * proof_wasm: add example web page to proof wasm
    * proof_wasm: make the browser calculate the time of generating proof
    * wallet_web: add a button for generating a proof
    
    Co-authored-by: Kihyun Kim <kihyun@saksaha.com>
    Co-authored-by: Aaron Cho <aaron@saksaha.com>

--- source/sak_proof_wasm/example/vite.config.ts
+++ source/sak_proof_wasm/example/vite.config.ts
@@ -6,5 +6,21 @@ export default defineConfig({
   plugins: [
     wasm(),
     topLevelAwait(),
+    {
+      name: "configure-response-headers",
+      configureServer: (server) => {
+        server.middlewares.use((_req, res, next) => {
+          res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+          res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
+          next();
+        });
+      },
+    },
   ],
+  server: {
+    fs: {
+      // Allow serving files from one level up to the project root
+      allow: ['../pkg/', '..'],
+    },
+  },
 });
