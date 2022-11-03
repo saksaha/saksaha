7772489c51be530d6cd06fc3127a81113a239999
Kihyun Kim <kihyun@saksaha.com>
2022-11-03 14:37:05 +0900

    delete wrong db files

--- source/sak_mrs/src/v0/tests/slot.rs
+++ source/sak_mrs/src/v0/tests/slot.rs
@@ -3,7 +3,7 @@ use crate::v0::db::{MrsEntity, MRSDB};
 
 #[tokio::test(flavor = "multi_thread")]
 async fn test_get_and_put_mrs_entity() {
-    MRSTestUtils::init_test(vec!["test"]);
+    // MRSTestUtils::init_test(vec!["test"]);
     let mrs_db = match MRSDB::init(&std::path::PathBuf::from("test")) {
         Ok(a) => a,
         Err(_) => panic!("panic"),
