192c77c3e63811a1cb680d733d3870966527d2a0
Aaron Cho <aaron@saksaha.com>
2022-11-07 15:40:19 +0900

    validator_contract+: make `validator` work (w/ macro to ctr_state)
    Co-authored-by: Jinwook Noah Jang <noah@saksaha.com>

--- source/sak_contract_std/src/v0/symbols.rs
+++ source/sak_contract_std/src/v0/symbols.rs
@@ -19,6 +19,9 @@ pub const CTR__UPDATE: &str = "CTR__update";
 pub const HOST__GET_MRS_DATA: &str = "HOST__get_mrs_data";
 
 #[allow(non_upper_case_globals)]
+pub const HOST__GET_CTR_STATE: &str = "HOST__get_ctr_state";
+
+#[allow(non_upper_case_globals)]
 pub const HOST__GET_LATEST_RETURN_LEN: &str = "HOST__get_latest_return_len";
 
 #[allow(non_upper_case_globals)]
